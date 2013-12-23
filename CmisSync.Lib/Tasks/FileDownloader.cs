f8c317659540cec2646536fb4247e1eb606f382a
Till Lorentzen <till.lorentzen@graudata.com>
2013-12-24 00:18:02 +0100

    Added a simple FileDownloader Task

--- CmisSync.Lib/Tasks/FileDownloader.cs
+++ CmisSync.Lib/Tasks/FileDownloader.cs
@@ -1,9 +1,31 @@
 using System;
+using System.IO;
+using DotCMIS.Client;
+using CmisSync.Lib.Events;
 
-namespace CmisSync.Lib
+namespace CmisSync.Lib.Tasks
 {
-	public interface DownloadFile
-	{
-	}
+    public interface FileDownloader
+    {
+        /// <summary>
+        /// Downloads the file and returns the SHA1 hash of the content of the saved file
+        /// </summary>
+        /// <returns>
+        /// SHA1 Hash of the file content
+        /// </returns>
+        /// <param name='remoteDocument'>
+        /// Remote document.
+        /// </param>
+        /// <param name='localFileStream'>
+        /// Local taget file stream.
+        /// </param>
+        /// <param name='TransmissionStatus'>
+        /// Transmission status.
+        /// </param>
+        /// <exception cref="IOException">On any disc or network io exception</exception>
+        /// <exception cref="DisposeException">If the remote object has been disposed before the dowload is finished</exception>
+        /// <exception cref="CmisException">On exceptions thrown by the CMIS Server/Client</exception>
+        byte[] DownloadFile (IDocument remoteDocument, Stream localFileStream, FileTransmissionEvent TransmissionStatus);
+    }
 }
 
--- CmisSync.Lib/Tasks/FileUploader.cs
+++ CmisSync.Lib/Tasks/FileUploader.cs
@@ -1,22 +1,26 @@
 using System;
 using DotCMIS.Client;
 using CmisSync.Lib.Events;
+
 namespace CmisSync.Lib
 {
-	namespace Tasks {
-		public abstract class FileUploader
-		{
-			private string localpath;
-			private IFolder remoteFolder;
-			private FileTransmissionEvent Status;
-			public FileUploader(string localpath, IFolder remoteFolder, FileTransmissionEvent TransmissionStatus){
-				if(String.IsNullOrEmpty(localpath))
-					throw new ArgumentException("Could not upload an empty local filepath");
-				if(remoteFolder == null)
-					throw new ArgumentNullException("Could not upload to a non existing IFolder instance");
-				Status = TransmissionStatus;
-			}
-		}
-	}
+    namespace Tasks
+    {
+        public abstract class FileUploader
+        {
+            private string localpath;
+            private IFolder remoteFolder;
+            private FileTransmissionEvent Status;
+
+            public FileUploader (string localpath, IFolder remoteFolder, FileTransmissionEvent TransmissionStatus)
+            {
+                if (String.IsNullOrEmpty (localpath))
+                    throw new ArgumentException ("Could not upload an empty local filepath");
+                if (remoteFolder == null)
+                    throw new ArgumentNullException ("Could not upload to a non existing IFolder instance");
+                Status = TransmissionStatus;
+            }
+        }
+    }
 }
 
