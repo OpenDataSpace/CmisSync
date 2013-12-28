c0204af7cd67d53d831fbd11e2dbb1f73b2eb673
Till Lorentzen <till.lorentzen@graudata.com>
2013-12-28 01:02:31 +0100

    Reverted unneeded IDisposable from AbstractFileFilter class

--- CmisSync.Lib/Events/EventFilter/AbstractFileFilter.cs
+++ CmisSync.Lib/Events/EventFilter/AbstractFileFilter.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace CmisSync.Lib.Events.Filter
 {
-    public abstract class AbstractFileFilter : SyncEventHandler, IDisposable
+    public abstract class AbstractFileFilter : SyncEventHandler
     {
         private static readonly int DEFAULT_FILTER_PRIORITY = 9999;
         protected readonly SyncEventQueue Queue;
@@ -11,10 +11,6 @@ namespace CmisSync.Lib.Events.Filter
             if( queue == null)
                 throw new ArgumentNullException("The given queue must not be null, bacause the Filters are reporting their filtered events to this queue");
             this.Queue = queue;
-        }
-
-        public void Dispose(){
-
         }
     }
 }
