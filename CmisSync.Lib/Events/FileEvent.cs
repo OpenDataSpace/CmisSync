d1237b3a242716c4c35b521c207c8e639c015a5b
Till Lorentzen <till.lorentzen@graudata.com>
2014-01-09 13:07:25 +0100

    Continue on File/FolderEvent implementation

--- CmisSync.Lib/Events/FileEvent.cs
+++ CmisSync.Lib/Events/FileEvent.cs
@@ -11,8 +11,21 @@ namespace CmisSync.Lib.Events
 
         public ChangeType RemoteContent { get; set; }
 
+        public FileInfo LocalFile { get; private set; }
+
+        public DirectoryInfo LocalParentDirectory { get; private set; }
+
+        public IDocument RemoteFile { get; private set; }
+
         public FileEvent (FileInfo localFile = null, DirectoryInfo localParentDirectory = null, IDocument remoteFile = null)
         {
+            if( localFile == null && remoteFile == null)
+                throw new ArgumentNullException("Given local or remote file must not be null");
+            this.LocalFile = localFile;
+            this.LocalParentDirectory = localParentDirectory;
+            this.RemoteFile = remoteFile;
+            this.LocalContent = ChangeType.NONE;
+            this.RemoteContent = ChangeType.NONE;
         }
 
         public override string ToString ()
--- CmisSync.Lib/Events/FolderEvent.cs
+++ CmisSync.Lib/Events/FolderEvent.cs
@@ -8,20 +8,41 @@ namespace CmisSync.Lib.Events
     public class FolderEvent : AbstractFolderEvent
     {
         public bool Recursive { get; set; }
+
+        public DirectoryInfo LocalFolder { get; private set; }
+
+        public IFolder RemoteFolder { get; private set; }
+
         public FolderEvent (DirectoryInfo localFolder = null, IFolder remoteFolder = null)
         {
+            if(localFolder == null && remoteFolder == null)
+                throw new ArgumentNullException("One of the given folders must not be null");
+            Recursive = false;
+            LocalFolder = localFolder;
+            RemoteFolder = remoteFolder;
         }
 
         public override string ToString ()
         {
-            return string.Format ("[FolderEvent: Local={0}, Remote={1}]", Local, Remote);
+            return string.Format ("[FolderEvent: Local={0} on {2}, Remote={1} on {3}]",
+                                  Local,
+                                  Remote,
+                                  LocalFolder != null ? LocalFolder.Name : "",
+                                  RemoteFolder.Name);
         }
     }
 
     public abstract class AbstractFolderEvent : ISyncEvent
     {
-        public ChangeType Local {get; set;}
-        public ChangeType Remote {get; set;}
+        public ChangeType Local { get; set; }
+
+        public ChangeType Remote { get; set; }
+
+        public AbstractFolderEvent ()
+        {
+            this.Local = ChangeType.NONE;
+            this.Remote = ChangeType.NONE;
+        }
     }
 }
 
