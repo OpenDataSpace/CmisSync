0a147f5f0c5d61741cf1376668a29e62f08a0872
Till Lorentzen <till.lorentzen@graudata.com>
2014-02-05 18:50:31 +0100

    Reduced the sync startup delay by 5 seconds

--- CmisSync.Lib/Sync/CmisRepo.cs
+++ CmisSync.Lib/Sync/CmisRepo.cs
@@ -44,6 +44,7 @@ namespace CmisSync.Lib.Sync
             this.Watcher.ChangeEvent += OnFileActivity;
             this.Watcher.EnableEvent = true;
             Logger.Info(synchronizedFolder);
+            SyncInBackground();
         }
 
         public override void Resume()
