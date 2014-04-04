1a8c341825ba4b9ac1edf825b2ed8848010ee978
more0401 <more0401@gmail.com>
2014-04-04 14:01:25 +0800

    Support CmisSync.Lib.ContentTasks.SimpleFileDownloader for abort operation
    (cherry picked from commit 22318e032d18ffed00fa176726852678ea65c524)
    
    Conflicts:
    	CmisSync.Lib/Events/ActiveActivitiesManager.cs

--- CmisSync.Lib/ContentTasks/SimpleFileDownloader.cs
+++ CmisSync.Lib/ContentTasks/SimpleFileDownloader.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using DotCMIS.Client;
 using System.Security.Cryptography;
 using System.IO;
@@ -38,6 +38,10 @@ namespace CmisSync.Lib.ContentTasks
                         if(this.disposed) {
                             TransmissionStatus.ReportProgress(new TransmissionProgressEventArgs(){Aborted = true});
                             throw new ObjectDisposedException("Disposed download task while downloading file: " + remoteDocument.Name);
+                        }
+                        if (TransmissionStatus.Status.Aborting.GetValueOrDefault()) {
+                            TransmissionStatus.ReportProgress(new TransmissionProgressEventArgs() { Aborting = false, Aborted = true });
+                            throw new AbortException(TransmissionStatus.Path + " is aborted");
                         }
                         hashstream.Write (buffer, 0, len);
                         hashstream.Flush();
