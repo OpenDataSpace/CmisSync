5dd77c7cf7bc82b1423f184489a6cbaacef926cc
Till Lorentzen <till.lorentzen@graudata.com>
2014-04-11 20:23:55 +0200

    Fixed a lot of style cop warnings in stream classes

--- CmisSync.Lib/Streams/ProgressStream.cs
+++ CmisSync.Lib/Streams/ProgressStream.cs
@@ -1,127 +1,273 @@
-﻿using System;
-using System.IO;
-using CmisSync.Lib.Events;
-using System.Timers;
+﻿//-----------------------------------------------------------------------
+// <copyright file="ProgressStream.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-----------------------------------------------------------------------
 
-namespace CmisSync.Lib
+namespace CmisSync.Lib.Streams
 {
-    namespace Streams
+    using System;
+    using System.IO;
+    using System.Timers;
+
+    using CmisSync.Lib.Events;
+
+    /// <summary>
+    /// Progress reporting stream.
+    /// </summary>
+    public class ProgressStream : StreamWrapper
     {
-        public class ProgressStream : StreamWrapper
+        /// <summary>
+        /// The transmission event which is used to report the status.
+        /// </summary>
+        private FileTransmissionEvent transmissionEvent;
+
+        /// <summary>
+        /// The start time of the usage.
+        /// </summary>
+        private DateTime start = DateTime.Now;
+
+        /// <summary>
+        /// The bytes transmitted since last second.
+        /// </summary>
+        private long bytesTransmittedSinceLastSecond = 0;
+
+        /// <summary>
+        /// The blocking detection timer.
+        /// </summary>
+        private Timer blockingDetectionTimer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CmisSync.Lib.Streams.ProgressStream"/> class.
+        /// The given transmission event will be used to report the progress
+        /// </summary>
+        /// <param name='stream'>
+        /// Stream which progress should be monitored.
+        /// </param>
+        /// <param name='e'>
+        /// Transmission event where the progress should be reported to.
+        /// </param>
+        public ProgressStream(Stream stream, FileTransmissionEvent e) : base(stream)
         {
-            private FileTransmissionEvent TransmissionEvent;
-            private DateTime start = DateTime.Now;
-            private long bytesTransmittedSinceLastSecond = 0;
-            private Timer blockingDetectionTimer;
-
-            public ProgressStream (Stream stream, FileTransmissionEvent e) : base (stream)
+            if (e == null)
             {
-                if (e == null)
-                    throw new ArgumentNullException ("The event, where to publish the prgress cannot be null");
-                try{
-                    e.Status.Length = stream.Length;
-                }catch(NotSupportedException){
-                    e.Status.Length = null;
-                }
-                TransmissionEvent = e;
-                blockingDetectionTimer = new Timer(2000);
-                blockingDetectionTimer.Elapsed += delegate(object sender, ElapsedEventArgs args) {
-                    this.TransmissionEvent.ReportProgress(new TransmissionProgressEventArgs() { BitsPerSecond = (long) (bytesTransmittedSinceLastSecond/blockingDetectionTimer.Interval) });
-                    bytesTransmittedSinceLastSecond = 0;
-               };
+                throw new ArgumentNullException("The event, where to publish the prgress cannot be null");
             }
-
-            #region overrideCode
-            public override long Length {
-                get {
-                    long length = this.Stream.Length;
-                    if (length > this.TransmissionEvent.Status.Length)
-                        this.TransmissionEvent.ReportProgress (new TransmissionProgressEventArgs () {Length = length});
-                    return length;
-                }
-            }
-
-            public override long Position {
-                get {
-                    long pos = this.Stream.Position;
-                    if (pos != this.TransmissionEvent.Status.ActualPosition)
-                        this.TransmissionEvent.ReportProgress (new TransmissionProgressEventArgs () {ActualPosition = pos});
-                    return pos;
-                }
-                set {
-                    this.Stream.Position = value;
-                    if (value != this.TransmissionEvent.Status.ActualPosition)
-                        this.TransmissionEvent.ReportProgress (new TransmissionProgressEventArgs () {ActualPosition = value});
-                }
-            }
-
-            public override long Seek (long offset, SeekOrigin origin)
+                
+            try
             {
-                long result = this.Stream.Seek (offset, origin);
-                this.TransmissionEvent.ReportProgress (new TransmissionProgressEventArgs () {ActualPosition = this.Stream.Position});
-                return result;
+                e.Status.Length = stream.Length;
             }
-
-            public override int Read (byte[] buffer, int offset, int count)
+            catch (NotSupportedException)
             {
-                if (TransmissionEvent.Status.Aborting.GetValueOrDefault()) {
-                    TransmissionEvent.ReportProgress(new TransmissionProgressEventArgs() { Aborting = false, Aborted = true });
-                    throw new ContentTasks.AbortException(TransmissionEvent.Path);
-                }
-                int result = this.Stream.Read(buffer, offset, count);
-                CalculateBandwidth(result);
-                return result;
+                e.Status.Length = null;
             }
 
-            public override void SetLength (long value)
+            this.transmissionEvent = e;
+            this.blockingDetectionTimer = new Timer(2000);
+            this.blockingDetectionTimer.Elapsed += delegate(object sender, ElapsedEventArgs args)
             {
-                this.Stream.SetLength (value);
-                if (this.TransmissionEvent.Status.Length == null || value > (long) this.TransmissionEvent.Status.Length)
-                    this.TransmissionEvent.ReportProgress (new TransmissionProgressEventArgs () {Length = value});
-            }
+                var transmissionArgs = new TransmissionProgressEventArgs
+                {
+                    BitsPerSecond = (long)(this.bytesTransmittedSinceLastSecond / this.blockingDetectionTimer.Interval)
+                };
 
-            public override void Write (byte[] buffer, int offset, int count)
+                this.transmissionEvent.ReportProgress(transmissionArgs);
+                this.bytesTransmittedSinceLastSecond = 0;
+            };
+        }
+
+        #region overrideCode
+        /// <summary>
+        /// Gets the length.
+        /// </summary>
+        /// <value>
+        /// The length.
+        /// </value>
+        public override long Length
+        {
+            get
             {
-                if (TransmissionEvent.Status.Aborting.GetValueOrDefault()) {
-                    TransmissionEvent.ReportProgress(new TransmissionProgressEventArgs() { Aborting = false, Aborted = true });
-                    throw new ContentTasks.AbortException(TransmissionEvent.Path);
+                long length = this.Stream.Length;
+                if (length > this.transmissionEvent.Status.Length)
+                {
+                    var transmissionArgs = new TransmissionProgressEventArgs
+                    {
+                        Length = length
+                    };
+                    this.transmissionEvent.ReportProgress(transmissionArgs);
                 }
-                this.Stream.Write(buffer, offset, count);
-                CalculateBandwidth(count);
-            }
-            #endregion
 
-            private void CalculateBandwidth(int transmittedBytes) {
-                this.bytesTransmittedSinceLastSecond+=transmittedBytes;
-                TimeSpan diff = DateTime.Now - start ;
-                long? pos;
-                try{
-                    pos = Stream.Position;
-                }catch (NotSupportedException) {
-                    pos = null;
+                return length;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the position.
+        /// </summary>
+        /// <value>
+        /// The position.
+        /// </value>
+        public override long Position
+        {
+            get
+            {
+                long pos = this.Stream.Position;
+                if (pos != this.transmissionEvent.Status.ActualPosition)
+                {
+                    this.transmissionEvent.ReportProgress(new TransmissionProgressEventArgs () {ActualPosition = pos});
                 }
-                if(diff.Seconds >= 1) {
-                    long? result = TransmissionProgressEventArgs.CalcBitsPerSecond(start, DateTime.Now, bytesTransmittedSinceLastSecond);
-                    this.TransmissionEvent.ReportProgress (new TransmissionProgressEventArgs () {ActualPosition = pos, BitsPerSecond = result});
-                    this.bytesTransmittedSinceLastSecond = 0;
-                    start = start + diff;
-                    blockingDetectionTimer.Stop();
-                    blockingDetectionTimer.Start();
-                }else{
-                    this.TransmissionEvent.ReportProgress (new TransmissionProgressEventArgs () {ActualPosition = pos});
-                }
+
+                return pos;
             }
 
-            /// <summary>
+            set
+            {
+                this.Stream.Position = value;
+                if (value != this.transmissionEvent.Status.ActualPosition)
+                {
+                    this.transmissionEvent.ReportProgress(new TransmissionProgressEventArgs () {ActualPosition = value});
+                }
+            }
+        }
+
+        /// <summary>
+        /// Seek the specified offset and origin.
+        /// </summary>
+        /// <param name='offset'>
+        /// Offset.
+        /// </param>
+        /// <param name='origin'>
+        /// Origin.
+        /// </param>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            long result = this.Stream.Seek(offset, origin);
+            this.transmissionEvent.ReportProgress(new TransmissionProgressEventArgs () {ActualPosition = this.Stream.Position});
+            return result;
+        }
+
+        /// <summary>
+        /// Read the specified buffer, offset and count.
+        /// </summary>
+        /// <param name='buffer'>
+        /// Buffer.
+        /// </param>
+        /// <param name='offset'>
+        /// Offset.
+        /// </param>
+        /// <param name='count'>
+        /// Count.
+        /// </param>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (this.transmissionEvent.Status.Aborting.GetValueOrDefault())
+            {
+                this.transmissionEvent.ReportProgress(new TransmissionProgressEventArgs{ Aborting = false, Aborted = true });
+                throw new ContentTasks.AbortException(this.transmissionEvent.Path);
+            }
+
+            int result = this.Stream.Read(buffer, offset, count);
+            this.CalculateBandwidth(result);
+            return result;
+        }
+
+        /// <summary>
+        /// Sets the length.
+        /// </summary>
+        /// <param name='value'>
+        /// Value.
+        /// </param>
+        public override void SetLength(long value)
+        {
+            this.Stream.SetLength(value);
+            if (this.transmissionEvent.Status.Length == null || value > (long)this.transmissionEvent.Status.Length)
+            {
+                this.transmissionEvent.ReportProgress(new TransmissionProgressEventArgs{Length = value});
+            }
+        }
+
+        /// <summary>
+        /// Write the specified buffer, offset and count.
+        /// </summary>
+        /// <param name='buffer'>
+        /// Buffer.
+        /// </param>
+        /// <param name='offset'>
+        /// Offset.
+        /// </param>
+        /// <param name='count'>
+        /// Count.
+        /// </param>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (this.transmissionEvent.Status.Aborting.GetValueOrDefault())
+            {
+                this.transmissionEvent.ReportProgress(new TransmissionProgressEventArgs() { Aborting = false, Aborted = true });
+                throw new ContentTasks.AbortException(this.transmissionEvent.Path);
+            }
+
+            this.Stream.Write(buffer, offset, count);
+            this.CalculateBandwidth(count);
+        }
+#endregion
+
+        /// <summary>
             /// Close this instance and calculates the bandwidth of the last second.
             /// </summary>
-            public override void Close ()
+        public override void Close()
+        {
+            long? result = TransmissionProgressEventArgs.CalcBitsPerSecond(this.start, DateTime.Now.AddMilliseconds(1), this.bytesTransmittedSinceLastSecond);
+            this.transmissionEvent.ReportProgress(new TransmissionProgressEventArgs() { BitsPerSecond = result });
+            base.Close();
+        }
+
+        /// <summary>
+        /// Calculates the bandwidth.
+        /// </summary>
+        /// <param name='transmittedBytes'>
+        /// Transmitted bytes.
+        /// </param>
+        private void CalculateBandwidth(int transmittedBytes)
+        {
+            this.bytesTransmittedSinceLastSecond += transmittedBytes;
+            TimeSpan diff = DateTime.Now - this.start;
+            long? pos;
+            try
             {
-                long? result = TransmissionProgressEventArgs.CalcBitsPerSecond(start, DateTime.Now.AddMilliseconds(1), bytesTransmittedSinceLastSecond);
-                this.TransmissionEvent.ReportProgress(new TransmissionProgressEventArgs() { BitsPerSecond = result });
-                base.Close ();
+                pos = Stream.Position;
+            }
+            catch (NotSupportedException)
+            {
+                pos = null;
+            }
+
+            if (diff.Seconds >= 1)
+            {
+                long? result = TransmissionProgressEventArgs.CalcBitsPerSecond(this.start, DateTime.Now, this.bytesTransmittedSinceLastSecond);
+                this.transmissionEvent.ReportProgress(new TransmissionProgressEventArgs{ActualPosition = pos, BitsPerSecond = result});
+                this.bytesTransmittedSinceLastSecond = 0;
+                this.start = this.start + diff;
+                this.blockingDetectionTimer.Stop();
+                this.blockingDetectionTimer.Start();
+            }
+            else
+            {
+                this.transmissionEvent.ReportProgress(new TransmissionProgressEventArgs{ActualPosition = pos});
             }
         }
     }
 }
-
--- CmisSync.Lib/Streams/StreamWrapper.cs
+++ CmisSync.Lib/Streams/StreamWrapper.cs
@@ -1,87 +1,244 @@
-using System;
-using System.IO;
+//-----------------------------------------------------------------------
+// <copyright file="StreamWrapper.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace CmisSync.Lib.Streams
 {
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// The stream wrapper class is used to hide a given stream and provide a wrapper to be
+    /// overridden by other stream extending classes without the need of the implementing class
+    /// to implement each function of the Stream interface
+    /// </summary>
     public class StreamWrapper : Stream
     {
-        protected Stream Stream;
-        public StreamWrapper (Stream stream)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CmisSync.Lib.Streams.StreamWrapper"/> class.
+        /// Takes a stream and wrapps all calls.
+        /// </summary>
+        /// <param name='stream'>
+        /// Stream to be wrapped.
+        /// </param>
+        public StreamWrapper(Stream stream)
         {
-            if(stream == null)
+            if (stream == null)
+            {
                 throw new ArgumentNullException("Given stream must not be null");
+            }
+                
             this.Stream = stream;
         }
 
         #region wrapperCode
-        public override bool CanRead {
-            get {
+        /// <summary>
+        /// Gets a value indicating whether the wrapped instance can read.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance can read; otherwise, <c>false</c>.
+        /// </value>
+        public override bool CanRead
+        {
+            get
+            {
                 return Stream.CanRead;
             }
         }
 
-        public override bool CanSeek {
-            get {
-                return Stream.CanSeek;
+        /// <summary>
+        /// Gets a value indicating whether the wrapped instance can seek.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance can seek; otherwise, <c>false</c>.
+        /// </value>
+        public override bool CanSeek
+        {
+            get
+            {
+                return this.Stream.CanSeek;
             }
         }
 
-        public override bool CanWrite {
-            get {
-                return Stream.CanWrite;
+        /// <summary>
+        /// Gets a value indicating whether the wrapped instance can write.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance can write; otherwise, <c>false</c>.
+        /// </value>
+        public override bool CanWrite
+        {
+            get
+            {
+                return this.Stream.CanWrite;
             }
         }
 
-        public override long Length {
-            get {
-                return Stream.Length;
+        /// <summary>
+        /// Gets the length of the the wrapped instance.
+        /// </summary>
+        /// <value>
+        /// The length.
+        /// </value>
+        public override long Length
+        {
+            get
+            {
+                return this.Stream.Length;
             }
         }
 
-        public override long Position {
-            get {
-                return Stream.Position;
+        /// <summary>
+        /// Gets or sets the position of the wrapped instance.
+        /// </summary>
+        /// <value>
+        /// The position.
+        /// </value>
+        public override long Position
+        {
+            get
+            {
+                return this.Stream.Position;
             }
-            set {
-                Stream.Position = value;
+
+            set
+            {
+                this.Stream.Position = value;
             }
         }
 
-        public override void Flush ()
+        /// <summary>
+        /// Gets or sets the stream.
+        /// </summary>
+        /// <value>
+        /// The stream.
+        /// </value>
+        protected Stream Stream { get; set; }
+
+        /// <summary>
+        /// Flush the wrapped instance.
+        /// </summary>
+        public override void Flush()
         {
-            Stream.Flush ();
+            this.Stream.Flush();
         }
 
-        public override IAsyncResult BeginRead (byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+        /// <summary>
+        /// Begins the read on the wrapped instance.
+        /// </summary>
+        /// <returns>
+        /// The result of the the wrapped instance call.
+        /// </returns>
+        /// <param name='buffer'>
+        /// Buffer passed to the wrapped instance.
+        /// </param>
+        /// <param name='offset'>
+        /// Offset passed to the wrapped instance.
+        /// </param>
+        /// <param name='count'>
+        /// Count passed to the wrapped instance.
+        /// </param>
+        /// <param name='callback'>
+        /// Callback passed to the wrapped instance.
+        /// </param>
+        /// <param name='state'>
+        /// State passed to the wrapped instance.
+        /// </param>
+        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
         {
-            return Stream.BeginRead (buffer, offset, count, callback, state);
+            return this.Stream.BeginRead(buffer, offset, count, callback, state);
         }
 
-        public override long Seek (long offset, SeekOrigin origin)
+        /// <summary>
+        /// Seeks on the wrapped instance with the specified offset and origin.
+        /// </summary>
+        /// <param name='offset'>
+        /// Offset passed to the wrapped instance.
+        /// </param>
+        /// <param name='origin'>
+        /// Origin passed to the wrapped instance.
+        /// </param>
+        /// <returns>
+        /// The result of the call passed to the wrapped instance.
+        /// </returns>
+        public override long Seek(long offset, SeekOrigin origin)
         {
-            return Stream.Seek (offset, origin);
+            return this.Stream.Seek(offset, origin);
         }
 
-        public override int Read (byte[] buffer, int offset, int count)
+        /// <summary>
+        /// Read the specified buffer, offset and count.
+        /// </summary>
+        /// <param name='buffer'>
+        /// Buffer passed to the wrapped instance.
+        /// </param>
+        /// <param name='offset'>
+        /// Offset passed to the wrapped instance.
+        /// </param>
+        /// <param name='count'>
+        /// Count passed to the wrapped instance.
+        /// </param>
+        /// <returns>
+        /// The result of the call passed to the wrapped instance.
+        /// </returns>
+        public override int Read(byte[] buffer, int offset, int count)
         {
-            return Stream.Read (buffer, offset, count);
+            return this.Stream.Read(buffer, offset, count);
         }
 
-        public override void SetLength (long value)
+        /// <summary>
+        /// Sets the length of the wrapped instance.
+        /// </summary>
+        /// <param name='value'>
+        /// Value passed to the wrapped instance.
+        /// </param>
+        public override void SetLength(long value)
         {
-            Stream.SetLength (value);
+            this.Stream.SetLength(value);
         }
 
-        public override void Write (byte[] buffer, int offset, int count)
+        /// <summary>
+        /// Write the specified buffer, offset and count.
+        /// </summary>
+        /// <param name='buffer'>
+        /// Buffer passed to the wrapped instance.
+        /// </param>
+        /// <param name='offset'>
+        /// Offset passed to the wrapped instance.
+        /// </param>
+        /// <param name='count'>
+        /// Count passed to the wrapped instance.
+        /// </param>
+        public override void Write(byte[] buffer, int offset, int count)
         {
-            Stream.Write (buffer, offset, count);
+            this.Stream.Write(buffer, offset, count);
         }
 
-        protected override void Dispose (bool disposing)
+        /// <summary>
+        /// Dispose the wrapper instance, not the wrapped stream object.
+        /// </summary>
+        /// <param name='disposing'>
+        /// Disposes the wrapper object.
+        /// </param>
+        protected override void Dispose(bool disposing)
         {
-            base.Dispose (disposing);
+            base.Dispose(disposing);
         }
         #endregion
     }
 }
-
