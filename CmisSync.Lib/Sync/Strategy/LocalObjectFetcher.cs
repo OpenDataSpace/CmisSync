37b72526b2920ece66f70c8af7fcaa7029f0b0aa
Frederick Roth <f-roth@megaera.de>
2014-04-17 15:46:27 +0200

    StyleCop

--- CmisSync.Lib/Sync/Strategy/LocalObjectFetcher.cs
+++ CmisSync.Lib/Sync/Strategy/LocalObjectFetcher.cs
@@ -1,3 +1,21 @@
+//-----------------------------------------------------------------------
+// <copyright file="LocalObjectFetcher.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-------------------
 namespace CmisSync.Lib.Sync.Strategy
 {
     using System;
@@ -6,36 +24,35 @@ namespace CmisSync.Lib.Sync.Strategy
     using CmisSync.Lib.Events;
     using CmisSync.Lib.Storage;
     
+    /// <summary>
+    /// Local object fetcher. I generates IFileSystemInfo if none found in FileEvent or FolderEvent
+    /// </summary>
+    /// <exception cref='ArgumentNullException'>
+    /// Is thrown when an argument passed to a method is invalid because it is <see langword="null" /> .
+    /// </exception>
     public class LocalObjectFetcher : SyncEventHandler
     {
         private IFileSystemInfoFactory fsFactory;
 
         private IPathMatcher matcher;
-
-        public override bool Handle(ISyncEvent e) {
-            if(e is FolderEvent){
-                var folderEvent = e as FolderEvent;
-                if(folderEvent.LocalFolder != null) {
-                    return false;
-                }
-                string localPath = matcher.CreateLocalPath(folderEvent.RemoteFolder.Path);
-                folderEvent.LocalFolder = fsFactory.CreateDirectoryInfo(localPath);
-            }
-            if(e is FileEvent){
-                var fileEvent = e as FileEvent;
-                if(fileEvent.LocalFile != null) {
-                    return false;
-                }
-                string localPath = matcher.CreateLocalPath(fileEvent.RemoteFile.Paths[0]);
-                fileEvent.LocalFile = fsFactory.CreateFileInfo(localPath);
-            }
-            return false;
-        }
-
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CmisSync.Lib.Sync.Strategy.LocalObjectFetcher"/> class.
+        /// </summary>
+        /// <param name='matcher'>
+        /// Matcher from IMetaDataStorage
+        /// </param>
+        /// <param name='fsFactory'>
+        /// Fs factory, should be null unless in Unit Tests
+        /// </param>
+        /// <exception cref='ArgumentNullException'>
+        /// Is thrown when an argument passed to a method is invalid because it is <see langword="null" /> .
+        /// </exception>
         public LocalObjectFetcher(IPathMatcher matcher, IFileSystemInfoFactory fsFactory = null) {
             if(matcher == null) {
                 throw new ArgumentNullException("matcher can not be null");
             }
+            
             this.matcher = matcher;
             if(fsFactory == null) {
                 this.fsFactory = new FileSystemInfoFactory();
@@ -43,6 +60,36 @@ namespace CmisSync.Lib.Sync.Strategy
                 this.fsFactory = fsFactory;
             }
         }
+  
+        /// <summary>
+        /// Handle the specified e if FolderEvent of FileEvent.
+        /// </summary>
+        /// <param name='e'>
+        /// Any ISyncEvent
+        /// </param>
+        /// <returns>always false</returns>
+        public override bool Handle(ISyncEvent e) {
+            if(e is FolderEvent) {
+                var folderEvent = e as FolderEvent;
+                if(folderEvent.LocalFolder != null) {
+                    return false;
+                }
+                
+                string localPath = this.matcher.CreateLocalPath(folderEvent.RemoteFolder.Path);
+                folderEvent.LocalFolder = this.fsFactory.CreateDirectoryInfo(localPath);
+            }
+            
+            if(e is FileEvent) {
+                var fileEvent = e as FileEvent;
+                if(fileEvent.LocalFile != null) {
+                    return false;
+                }
+                
+                string localPath = this.matcher.CreateLocalPath(fileEvent.RemoteFile.Paths[0]);
+                fileEvent.LocalFile = this.fsFactory.CreateFileInfo(localPath);
+            }
+            
+            return false;
+        }
     }
 }
-
