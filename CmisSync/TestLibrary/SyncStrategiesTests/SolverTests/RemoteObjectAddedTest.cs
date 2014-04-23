66b7322f01c2cdfde32cf06ed9cf95f29ae0dd47
Frederick Roth <f-roth@megaera.de>
2014-04-23 13:08:37 +0200

    BROKEN: Commited test for till to look at

--- CmisSync/TestLibrary/SyncStrategiesTests/SolverTests/RemoteObjectAddedTest.cs
+++ CmisSync/TestLibrary/SyncStrategiesTests/SolverTests/RemoteObjectAddedTest.cs
@@ -10,6 +10,8 @@ namespace TestLibrary.SyncStrategiesTests.SolverTests
 
     using NUnit.Framework;
 
+    using TestLibrary.TestUtils;
+
     using Moq;
 
     [TestFixture]
@@ -25,6 +27,9 @@ namespace TestLibrary.SyncStrategiesTests.SolverTests
         public void RemoteFolderAdded()
         {
             string path = Path.Combine(Path.GetTempPath(), "a");
+            string id = "id";
+            string parentId = "papa";
+            string lastChangeToken = "token";
             var session = new Mock<ISession>();
 
             var storage = new Mock<IMetaDataStorage>();
@@ -32,12 +37,21 @@ namespace TestLibrary.SyncStrategiesTests.SolverTests
             var dirInfo = new Mock<IDirectoryInfo>();
             dirInfo.Setup(d => d.FullName).Returns(path);
 
+            Mock<IFolder> remoteObject = MockSessionUtil.CreateRemoteFolderMock(id, path, parentId, lastChangeToken);
+
             var solver = new RemoteObjectAdded();
             
-            solver.Solve(session.Object, storage.Object, dirInfo.Object, null);
+            solver.Solve(session.Object, storage.Object, dirInfo.Object, remoteObject.Object);
             dirInfo.Verify(d => d.Create(), Times.Once());
-            Assert.Fail("verify that folder goes to db");
+            storage.Verify(s => s.SaveMappedObject(It.IsAny<IMappedObject>()), Times.Once());
+/*            storage.Verify(s => s.SaveMappedObject(It.Is<IMappedObject>(f =>
+                            f.RemoteObjectId == id &&
+                            f.Name == folderName &&
+                            f.ParentId == parentId &&
+                            f.LastChangeToken == lastChangeToken &&
+                            f.Type == MappedObjectType.Folder)
+                    ), Times.Once());
+                    */
         }
     }
 }
-
