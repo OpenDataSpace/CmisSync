c9b438fdf9600fb7557fa162bafea470b0924690
Frederick Roth <f-roth@megaera.de>
2014-04-24 11:07:26 +0200

    Moved EventTypes to own folder

--- CmisSync.Lib/Events/EventTypes/FSEvent.cs
+++ CmisSync.Lib/Events/EventTypes/FSEvent.cs
@@ -34,15 +34,5 @@ namespace CmisSync.Lib.Events
             }
         }
     }
-
-    public class FSMovedEvent : FSEvent
-    {
-        public virtual string OldPath { get; private set; }
-
-        public FSMovedEvent (string oldPath, string newPath) : base(WatcherChangeTypes.Renamed, newPath)
-        {
-            OldPath = oldPath;
-        }
-    }
 }
 
