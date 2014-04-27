e31cd29e3be871e47093cdf8230d9ccb41413348
Till Lorentzen <till.lorentzen@graudata.com>
2014-04-27 11:10:11 +0200

    StyleCop fixes

--- CmisSync.Lib/ContentTasks/Utils.cs
+++ CmisSync.Lib/ContentTasks/Utils.cs
@@ -16,39 +16,68 @@
 //
 // </copyright>
 //-----------------------------------------------------------------------
-using System;
-using System.IO;
-using System.Security.Cryptography;
 
 namespace CmisSync.Lib.ContentTasks
 {
-    public static class ContentTaskUtils {
-        public static IFileUploader CreateUploader (long chunkSize = 0)
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+
+    /// <summary>
+    /// Content task utils.
+    /// </summary>
+    public static class ContentTaskUtils
+    {
+        /// <summary>
+        /// Creates the matching uploader.
+        /// </summary>
+        /// <returns>The uploader.</returns>
+        /// <param name="chunkSize">Chunk size.</param>
+        public static IFileUploader CreateUploader(long chunkSize = 0)
         {
             if (chunkSize > 0)
+            {
                 return new ChunkedUploader(chunkSize);
+            }
+
             return new SimpleFileUploader();
         }
 
-        public static void PrepareResume(long successfulLength, System.IO.Stream successfulPart, HashAlgorithm hashAlg)
+        /// <summary>
+        /// Prepares to resume.
+        /// </summary>
+        /// <param name="successfulLength">Successful length.</param>
+        /// <param name="successfulPart">Successful part.</param>
+        /// <param name="hashAlg">Hash algorithm</param>
+        public static void PrepareResume(long successfulLength, Stream successfulPart, HashAlgorithm hashAlg)
         {
             byte[] buffer = new byte[4096];
             int pos = 0;
             while(pos < successfulLength)
             {
-                int l = successfulPart.Read(buffer, 0, (int) Math.Min(buffer.Length, successfulLength - pos));
-                if(l<=0) {
-                    throw new IOException(String.Format("File stream is shorter ({0}) than the given length {1}", pos, successfulLength));
+                int l = successfulPart.Read(buffer, 0, (int)Math.Min(buffer.Length, successfulLength - pos));
+                if (l <= 0)
+                {
+                    throw new IOException(string.Format("File stream is shorter ({0}) than the given length {1}", pos, successfulLength));
                 }
-                hashAlg.TransformBlock (buffer, 0, l, buffer, 0);
+
+                hashAlg.TransformBlock(buffer, 0, l, buffer, 0);
                 pos += l;
             }
         }
 
-        public static IFileDownloader CreateDownloader (long chunkSize = 0)
+        /// <summary>
+        /// Creates the downloader.
+        /// </summary>
+        /// <returns>The downloader.</returns>
+        /// <param name="chunkSize">Chunk size.</param>
+        public static IFileDownloader CreateDownloader(long chunkSize = 0)
         {
             if(chunkSize > 0)
+            {
                 return new ChunkedDownloader(chunkSize);
+            }
+
             return new SimpleFileDownloader();
         }
     }
