74438610d7b02e9f6c6bf5a3d2802ca54ec49607
Frederick Roth <f-roth@megaera.de>
2014-05-15 14:45:48 +0200

    StyleCop

--- CmisSync.Lib/Events/EventTypes/IFSEvent.cs
+++ CmisSync.Lib/Events/EventTypes/IFSEvent.cs
@@ -1,14 +1,52 @@
+//-----------------------------------------------------------------------
+// <copyright file="IFSEvent.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-----------------------------------------------------------------------
 namespace CmisSync.Lib.Events
 {
     using System.IO;
     
+    /// <summary>
+    /// Interface for FileSystem Events.
+    /// </summary>
     public interface IFSEvent : ISyncEvent
     {
+        /// <summary>
+        /// Gets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
         WatcherChangeTypes Type { get; }
-
+  
+        /// <summary>
+        /// Gets the path.
+        /// </summary>
+        /// <value>
+        /// The path.
+        /// </value>
         string Path { get; }
-
+  
+        /// <summary>
+        /// Determines whether this instance is directory.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> if this instance is directory; otherwise, <c>false</c>.
+        /// </returns>
         bool IsDirectory();
     }
 }
-
