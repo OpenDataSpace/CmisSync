25ffaab02d48c1755ce8b64c6e9149412a17014c
Frederick Roth <f-roth@megaera.de>
2014-05-15 13:14:49 +0200

    StyleCop

--- CmisSync.Lib/Streams/ChunkedStream.cs
+++ CmisSync.Lib/Streams/ChunkedStream.cs
@@ -16,57 +16,61 @@
 //
 // </copyright>
 //-----------------------------------------------------------------------
-using System;
-using System.Diagnostics;
-using System.IO;
-
 namespace CmisSync.Lib
 {
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
     /// <summary>
     /// Chunked stream.
     /// </summary>
     public class ChunkedStream : Stream
     {
+        private long chunkPosition;
         private Stream source;
         private long chunkSize;
+        private long position;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CmisSync.Lib.ChunkedStream"/> class.
         /// </summary>
-        /// <param name="stream">Stream.</param>
-        /// <param name="chunk">Chunk.</param>
+        /// <param name="stream">Stream to chunk.</param>
+        /// <param name="chunk">The chunksize.</param>
         public ChunkedStream(Stream stream, long chunk)
         {
-            source = stream;
-            chunkSize = chunk;
-
-            //if (!source.CanRead)
-            //{
-            //    throw new System.NotSupportedException("Read access is needed for ChunkedStream");
-            //}
+            this.source = stream;
+            this.chunkSize = chunk;
+  ////if (!source.CanRead)
+            ////{
+            ////    throw new System.NotSupportedException("Read access is needed for ChunkedStream");
+            ////}
         }
 
         /// <summary>
         /// Gets a value indicating whether this and the source stream can be read.
         /// </summary>
         /// <value><c>true</c> if this instance can read; otherwise, <c>false</c>.</value>
-        public override bool CanRead { get { return source.CanRead; } }
+        public override bool CanRead { get
+            { return this.source.CanRead; }
+        }
+        
         /// <summary>
         /// Gets a value indicating whether this and the source stream can written.
         /// </summary>
         /// <value><c>true</c> if this instance can write; otherwise, <c>false</c>.</value>
-        public override bool CanWrite { get { return source.CanWrite; } }
+        public override bool CanWrite { get
+            { return this.source.CanWrite; }
+        }
+        
         /// <summary>
         /// Gets a value indicating whether this and the source stream are able to be seeked.
         /// </summary>
         /// <value><c>true</c> if this instance can seek; otherwise, <c>false</c>.</value>
-        public override bool CanSeek { get { return source.CanSeek; } }
-        /// <summary>
-        /// Flush all data of the source stream.
-        /// </summary>
-        public override void Flush() { source.Flush(); }
+        public override bool CanSeek {
+            get { return this.source.CanSeek; }
+        }
 
-        private long chunkPosition;
         /// <summary>
         /// Gets or sets the chunk position.
         /// </summary>
@@ -75,13 +79,13 @@ namespace CmisSync.Lib
         {
             get
             {
-                return chunkPosition;
+                return this.chunkPosition;
             }
 
             set
             {
-                source.Position = value;
-                chunkPosition = value;
+                this.source.Position = value;
+                this.chunkPosition = value;
             }
         }
 
@@ -93,16 +97,16 @@ namespace CmisSync.Lib
         {
             get
             {
-                long lengthSource = source.Length;
-                if (lengthSource <= ChunkPosition)
+                long lengthSource = this.source.Length;
+                if (lengthSource <= this.ChunkPosition)
                 {
                     return 0;
                 }
 
-                long length = lengthSource - ChunkPosition;
-                if (length >= chunkSize)
+                long length = lengthSource - this.ChunkPosition;
+                if (length >= this.chunkSize)
                 {
-                    return chunkSize;
+                    return this.chunkSize;
                 }
                 else
                 {
@@ -111,7 +115,6 @@ namespace CmisSync.Lib
             }
         }
 
-        private long position;
         /// <summary>
         /// Gets or sets the position in the chunk.
         /// </summary>
@@ -120,97 +123,117 @@ namespace CmisSync.Lib
         {
             get
             {
-                if (!CanSeek)
+                if (!this.CanSeek)
                 {
-                    return position;
+                    return this.position;
                 }
 
-                long offset = source.Position - ChunkPosition;
-                if (offset < 0 || offset > chunkSize)
+                long offset = this.source.Position - this.ChunkPosition;
+                if (offset < 0 || offset > this.chunkSize)
                 {
-                    Debug.Assert(false, String.Format("Position {0} not in [0,{1}]", offset, chunkSize));
+                    Debug.Assert(false, string.Format("Position {0} not in [0,{1}]", offset, this.chunkSize));
                 }
+                
                 return offset;
             }
 
             set
             {
-                if (value < 0 || value > chunkSize)
+                if (value < 0 || value > this.chunkSize)
                 {
-                    throw new System.ArgumentOutOfRangeException(String.Format("Position {0} not in [0,{1}]", value, chunkSize));
+                    throw new System.ArgumentOutOfRangeException(string.Format("Position {0} not in [0,{1}]", value, this.chunkSize));
                 }
-                source.Position = ChunkPosition + value;
+                
+                this.source.Position = this.ChunkPosition + value;
             }
         }
+        
+        /// <summary>
+        /// Flush all data of the source stream.
+        /// </summary>
+        public override void Flush() 
+        {
+            this.source.Flush(); 
+        }
+
         /// <summary>
         /// Read the specified buffer from the given offset and with the length of count.
         /// </summary>
-        /// <param name="buffer">Buffer.</param>
-        /// <param name="offset">Offset.</param>
-        /// <param name="count">Count.</param>
+        /// <param name="buffer">The buffer to read.</param>
+        /// <param name="offset">Offset to start reading.</param>
+        /// <param name="count">Count of bytes.</param>
+        /// <returns>
+        /// bytes read
+        /// </returns>
         public override int Read(byte[] buffer, int offset, int count)
         {
             if (offset < 0)
             {
                 throw new System.ArgumentOutOfRangeException("offset", offset, "offset is negative");
             }
+            
             if (count < 0)
             {
                 throw new System.ArgumentOutOfRangeException("count", count, "count is negative");
             }
 
-            if (count > chunkSize - Position)
+            if (count > this.chunkSize - this.Position)
             {
-                count = (int)(chunkSize - Position);
+                count = (int)(this.chunkSize - this.Position);
             }
-            count = source.Read(buffer, offset, count);
-            position += count;
+            
+            count = this.source.Read(buffer, offset, count);
+            this.position += count;
             return count;
         }
 
         /// <summary>
         /// Write the specified buffer from the given offset and the count.
         /// </summary>
-        /// <param name="buffer">Buffer.</param>
-        /// <param name="offset">Offset.</param>
-        /// <param name="count">Count.</param>
+        /// <param name="buffer">The buffer to write.</param>
+        /// <param name="offset">Offset to start writing.</param>
+        /// <param name="count">Count of bytes.</param>
         public override void Write(byte[] buffer, int offset, int count)
         {
             if (offset < 0)
             {
                 throw new System.ArgumentOutOfRangeException("offset", offset, "offset is negative");
             }
+            
             if (count < 0)
             {
                 throw new System.ArgumentOutOfRangeException("count", count, "count is negative");
             }
 
-            if (count > chunkSize - Position)
+            if (count > this.chunkSize - this.Position)
             {
                 throw new System.ArgumentOutOfRangeException("count", count, "count is overflow");
             }
-            source.Write(buffer, offset, count);
-            position += count;
+            
+            this.source.Write(buffer, offset, count);
+            this.position += count;
         }
 
         /// <summary>
         /// Seek the specified offset and origin.
         /// </summary>
-        /// <param name="offset">Offset.</param>
-        /// <param name="origin">Origin.</param>
+        /// <param name="offset">The Offset.</param>
+        /// <param name="origin">The Origin.</param>
+        /// <returns>the found position</returns>
         public override long Seek(long offset, SeekOrigin origin)
         {
             Debug.Assert(false, "TODO");
-            return source.Seek(offset, origin);
+            return this.source.Seek(offset, origin);
         }
+        
         /// <summary>
         /// Sets the length. Is not implemented at correctly. It simply passes the call to the source stream.
         /// </summary>
-        /// <param name="value">Value.</param>
+        /// <param name="value">The length to set.</param>
         public override void SetLength(long value)
         {
             Debug.Assert(false, "TODO");
-            source.SetLength(value);
+            this.source.SetLength(value);
         }
     }
 }
