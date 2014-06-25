e80c0fb87390fc8fb79faf979f2ee7a670c919db
Till Lorentzen <till.lorentzen@graudata.com>
2014-06-25 17:21:02 +0200

    Second try to fix the remote path creation for full repo test

--- CmisSync/TestLibrary/IntegrationTests/FullRepoTests.cs
+++ CmisSync/TestLibrary/IntegrationTests/FullRepoTests.cs
@@ -112,11 +112,12 @@ namespace TestLibrary.IntegrationTests
             this.repoInfo = new RepoInfo {
                 AuthenticationType = AuthenticationType.BASIC,
                 LocalPath = Path.Combine(config[1].ToString(), Subfolder),
-                RemotePath = config[2].ToString().EndsWith("/") ? config[2].ToString() + Subfolder : config[2].ToString() + "/" + Subfolder,
+                RemotePath = config[2].ToString() + "/" + Subfolder,
                 Address = new XmlUri(new Uri(config[3].ToString())),
                 User = config[4].ToString(),
                 RepositoryId = config[6].ToString()
             };
+            this.repoInfo.RemotePath = this.repoInfo.RemotePath.Replace("//", "/");
             this.repoInfo.SetPassword(config[5].ToString());
 
             // FileSystemDir
