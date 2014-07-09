5a0a8c7b6da4a37f832da7d06864eb6fc63bb51f
Frederick Roth <f-roth@megaera.de>
2014-07-09 14:19:13 +0200

    Added Solver/LocalObjectDeletedRemoteObjectDeleted

---
+++ CmisSync.Lib/Sync/Solver/LocalObjectDeletedRemoteObjectDeleted.cs
@@ -0,0 +1,36 @@
+//-----------------------------------------------------------------------
+// <copyright file="LocalObjectDeletedRemoteObjectDeleted.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+namespace CmisSync.Lib.Sync.Solver
+{
+    using CmisSync.Lib.Storage;
+    using DotCMIS.Client;
+
+    /// <summary>
+    /// Local object deleted remote object deleted.
+    /// </summary>
+    public class LocalObjectDeletedRemoteObjectDeleted : ISolver
+    {
+        public virtual void Solve(ISession session, IMetaDataStorage storage, IFileSystemInfo localFile, IObjectId remoteId)
+        {
+            string id = remoteId.Id;
+            var mappedObject = storage.GetObjectByRemoteId(id);
+            storage.RemoveObject(mappedObject);
+        }
+    }
+}
---
+++ CmisSync/TestLibrary/SyncStrategiesTests/SolverTests/LocalObjectDeletedRemoteObjectDeletedTest.cs
@@ -0,0 +1,68 @@
+//-----------------------------------------------------------------------
+// <copyright file="LocalObjectDeletedRemoteObjectDeletedTest.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace TestLibrary.SyncStrategiesTests.SolverTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    using CmisSync.Lib.Data;
+    using CmisSync.Lib.Storage;
+    using CmisSync.Lib.Sync.Solver;
+
+    using DotCMIS.Client;
+    using DotCMIS.Data;
+    using DotCMIS.Enums;
+    using DotCMIS.Exceptions;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    using TestLibrary.TestUtils;
+
+    [TestFixture]
+    public class LocalObjectDeletedRemoteObjectDeletedTest
+    {
+        [Test, Category("Fast"), Category("Solver")]
+        public void DefaultConstructorTest()
+        {
+            new LocalObjectDeletedRemoteObjectDeleted();
+        }
+
+        [Test, Category("Fast"), Category("Solver")]
+        public void LocalFileDeleted()
+        {
+            var session = new Mock<ISession>();
+            var storage = new Mock<IMetaDataStorage>();
+            string tempFile = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
+
+            string remoteDocumentId = "DocumentId";
+
+            var docId = new Mock<IObjectId>();
+            docId.Setup(d => d.Id).Returns(remoteDocumentId);
+            storage.AddLocalFile(tempFile, remoteDocumentId);
+
+            new LocalObjectDeletedRemoteObjectDeleted().Solve(session.Object, storage.Object, new FileSystemInfoFactory().CreateFileInfo(tempFile), docId.Object);
+
+            storage.Verify(s => s.RemoveObject(It.Is<IMappedObject>(o => o.RemoteObjectId == remoteDocumentId)), Times.Once());
+        }
+    }
+}
