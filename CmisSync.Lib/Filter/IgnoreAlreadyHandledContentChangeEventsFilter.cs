83c655f44a87d0d69ac1af0222795255a59b8147
Till Lorentzen <till.lorentzen@graudata.com>
2014-07-28 15:50:50 +0200

    Reverted Hack to avoid invalid events

--- CmisSync.Lib/Filter/IgnoreAlreadyHandledContentChangeEventsFilter.cs
+++ CmisSync.Lib/Filter/IgnoreAlreadyHandledContentChangeEventsFilter.cs
@@ -71,10 +71,7 @@ namespace CmisSync.Lib.Filter
                 ContentChangeEvent change = e as ContentChangeEvent;
                 switch (change.Type) {
                 case ChangeType.Created:
-                    // HACK This should be taken if the server sends correct change events
-                    // return this.storage.GetObjectByRemoteId(change.ObjectId) != null;
-                    goto case ChangeType.Updated;
-                    // HACK end
+                    return this.storage.GetObjectByRemoteId(change.ObjectId) != null;
                 case ChangeType.Deleted:
                     return this.storage.GetObjectByRemoteId(change.ObjectId) == null;
                 case ChangeType.Security:
