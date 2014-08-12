d9b9a3edde2d98ba3e60da5d9ae7209f272e3a6c
Frederick Roth <f-roth@megaera.de>
2014-08-12 11:27:24 +0200

    Empty classes DelayRetryAndNextSyncEventHandler

---
+++ CmisSync/TestLibrary/QueueingTests/DelayRetryAndNextSyncEventHandlerTest.cs
@@ -0,0 +1,34 @@
+//-----------------------------------------------------------------------
+// <copyright file="DelayRetryAndNextSyncEventHandlerTest.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+namespace TestLibrary.QueueingTests
+{
+    using CmisSync.Lib.Queueing;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DelayRetryAndNextSyncEventHandlerTest
+    {
+        [Test, Category("Fast")]
+        public void ConstructorTest() {
+            var underTest = new DelayRetryAndNextSyncEventHandler();
+        }
+    }
+}
+
