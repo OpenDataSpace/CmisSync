2f31ddfd07557f6dfd8eb8db991941860fec4419
more0401 <more0401@gmail.com>
2014-09-15 11:42:39 +0800

    Fix mantis issue 4183: Remote folder Settings window does not close when the remote folder is removed

--- CmisSync/ControllerBase.cs
+++ CmisSync/ControllerBase.cs
@@ -407,7 +407,7 @@ namespace CmisSync
                     }
                 };
 
-                edit.Controller.CloseWindowEvent += delegate
+                edit.Controller.CleanWindowEvent += delegate
                 {
                     lock (this.repo_lock)
                     {
--- CmisSync/Windows/Edit.cs
+++ CmisSync/Windows/Edit.cs
@@ -116,7 +116,7 @@ namespace CmisSync
 
             Controller.CloseWindowEvent += delegate
             {
-                asyncLoader.Cancel();
+                Close();
             };
 
             finishButton.Click += delegate
@@ -124,21 +124,25 @@ namespace CmisSync
                 Ignores = NodeModelUtils.GetIgnoredFolder(repo);
                 Credentials.Password = passwordBox.Password;
                 Controller.SaveFolder();
-                Close();
+                Controller.CloseWindow();
             };
 
             cancelButton.Click += delegate
             {
-                Close();
+                Controller.CloseWindow();
+            };
+
+            Closed += delegate
+            {
+                Controller.CleanWindow();
             };
         }
 
-
         protected override void Close(object sender, CancelEventArgs args)
         {
+            asyncLoader.Cancel();
             backgroundWorker.CancelAsync();
             backgroundWorker.Dispose();
-            Controller.CloseWindow();
         }
 
 
