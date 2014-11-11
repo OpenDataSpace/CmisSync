e95b095552dd0b8daa7f2b4d7d481d308a53cb32
Till Lorentzen <till.lorentzen@graudata.com>
2014-11-11 17:26:24 +0100

    Whitespace and icon path fixes

--- CmisSync/Linux/Notification/NotificationUtils.cs
+++ CmisSync/Linux/Notification/NotificationUtils.cs
@@ -25,38 +25,6 @@ namespace CmisSync.Notifications
 {
     public static class NotificationUtils
     {
-//        /// <summary>
-//        /// Creates a Notification by calling the external process "notify-send"
-//        /// </summary>
-//        /// <param name='title'>
-//        /// Title.
-//        /// </param>
-//        /// <param name='content'>
-//        /// Content.
-//        /// </param>
-//        /// <param name='iconPath'>
-//        /// Icon path.
-//        /// </param>
-//        public static void NotifyAsync(string title, string content = null, string iconPath = null)
-//        {
-//            string IconPath = Path.Combine("/","usr","share","icons","hicolor", "32x32", "apps", "app-cmissync.png");
-//            if(!String.IsNullOrEmpty(iconPath))
-//            {
-//                IconPath = iconPath;
-//            }
-//            Process process = new Process();
-//            process.StartInfo.FileName  = "notify-send";
-//            if(String.IsNullOrEmpty(content))
-//            {
-//                process.StartInfo.Arguments = String.Format("-i \"{0}\" \"{1}\"", IconPath, title);
-//            }
-//            else
-//            {
-//                process.StartInfo.Arguments = String.Format("-i \"{0}\" \"{1}\" \"{2}\"", IconPath, title, content);
-//            }
-//            process.Start();
-//        }
-
         static private Notification notification = new Notification();
         static private object notificationLock = new object();
 
@@ -74,19 +42,16 @@ namespace CmisSync.Notifications
         /// </param>
         public static void NotifyAsync(string title, string content = null, string iconPath = null)
         {
-            string IconPath = Path.Combine("/","usr","share","icons","hicolor", "32x32", "apps", "app-cmissync.png");
-            if(!String.IsNullOrEmpty(iconPath))
-            {
+            string IconPath = Path.Combine("/","usr","share","icons","hicolor", "32x32", "apps", "dataspacesync-app.png");
+            if (!String.IsNullOrEmpty(iconPath)) {
                 IconPath = iconPath;
             }
 
-            if(content==null)
-            {
+            if (content==null) {
                 content = string.Empty;
             }
 
-            lock(notificationLock)
-            {
+            lock(notificationLock) {
                 notification.Summary = title;
                 notification.Body = content;
                 notification.Show();
@@ -94,4 +59,3 @@ namespace CmisSync.Notifications
         }
     }
 }
-
