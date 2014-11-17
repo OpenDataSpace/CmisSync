fefa639d5e14478e1327c7502dbd9e3674fbea6c
Till Lorentzen <till.lorentzen@graudata.com>
2014-11-17 15:27:08 +0100

    Added proxy widget template

---
+++ CmisSync/Linux/ProxyWidget.cs
@@ -0,0 +1,13 @@
+
+namespace CmisSync.Widgets
+{
+    using System;
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class ProxyWidget : Gtk.Bin
+    {
+        public ProxyWidget()
+        {
+            this.Build();
+        }
+    }
+}
