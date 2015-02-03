73333a70e374e4714b334aca2067df44f143db50
Till Lorentzen <till.lorentzen@graudata.com>
2015-02-03 16:18:00 +0100

    Removed Whitespaces

--- CmisSync/ControllerBase.cs
+++ CmisSync/ControllerBase.cs
@@ -312,8 +312,7 @@ namespace CmisSync {
                         string pathname = Path.Combine(config.GetConfigPath(), this.BrandConfigFolder, path.Substring(1));
                         Directory.CreateDirectory(Path.GetDirectoryName(pathname));
                         try {
-                            using (FileStream output = File.OpenWrite(pathname))
-                            {
+                            using (FileStream output = File.OpenWrite(pathname)) {
                                 if (!clientBrand.GetFile(path, output)) {
                                     success = false;
                                     break;
@@ -370,8 +369,7 @@ namespace CmisSync {
             if (this.firstRun) {
                 this.ShowSetupWindow(PageType.Setup);
             } else {
-                new Thread(() =>
-                {
+                new Thread(() => {
                     CheckRepositories();
                     RepositoriesLoaded = true;
 
