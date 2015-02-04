76feaa97b94a51f0d316d20c1897433742fc390d
Till Lorentzen <till.lorentzen@graudata.com>
2015-02-04 01:10:37 +0100

    Fixed compile errors on MacOS branding classes

--- CmisSync/Mac/UIHelpers.cs
+++ CmisSync/Mac/UIHelpers.cs
@@ -16,8 +16,7 @@
 //
 // </copyright>
 //-----------------------------------------------------------------------
-﻿namespace CmisSync
-{
+﻿namespace CmisSync {
     using System;
     using System.IO;
 
@@ -25,41 +24,38 @@
 
     using CmisSync.Lib.Config;
 
-    public static class UIHelpers
-    {
-        public static string GetImagePathname (string name, string type = "png")
-        {
+    public static class UIHelpers {
+        public static string GetImagePathname(string name, string type = "png") {
             string filename = name + "." + type;
 
-            string brandFolder = Path.Combine (ConfigManager.CurrentConfig.GetConfigPath (), Program.Controller.BrandConfigFolder);
-            string pathname = FindImagePathname (brandFolder, filename);
-            if (!string.IsNullOrEmpty (pathname)) {
+            string brandFolder = Path.Combine(ConfigManager.CurrentConfig.GetConfigPath(), Program.Controller.BrandConfigFolder);
+            string pathname = FindImagePathname(brandFolder, filename);
+            if (!string.IsNullOrEmpty(pathname)) {
                 return pathname;
             }
 
-            pathname = Path.Combine (NSBundle.MainBundle.ResourcePath, "Pixmaps", filename);
-            if (File.Exists (pathname)) {
+            pathname = Path.Combine(NSBundle.MainBundle.ResourcePath, "Pixmaps", filename);
+            if (File.Exists(pathname)) {
                 return pathname;
             }
 
-            return Path.Combine (NSBundle.MainBundle.ResourcePath, filename);
+            return Path.Combine(NSBundle.MainBundle.ResourcePath, filename);
         }
 
-        private static string FindImagePathname (string folder, string filename)
-        {
-            ClientBrand brand = new ClientBrand ();
-            foreach (string path in brand.GetPathList()) {
-                if (Path.GetFileName (path) == filename) {
-                    string pathname = Path.Combine (folder, path.Substring(1));
-                    if (File.Exists (pathname)) {
+        private static string FindImagePathname(string folder, string filename) {
+            ClientBrand brand = new ClientBrand();
+            foreach (string path in brand.PathList) {
+                if (Path.GetFileName(path) == filename) {
+                    string pathname = Path.Combine(folder, path.Substring(1));
+                    if (File.Exists(pathname)) {
                         return pathname;
                     } else {
                         return null;
                     }
                 }
             }
+
             return null;
         }
     }
 }
-
