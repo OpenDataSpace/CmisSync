a810f63eeb444b6327d718a803f8897715e39cdf
Till Lorentzen <till.lorentzen@graudata.com>
2015-02-11 17:42:26 +0100

    Fixed wrong close button title

--- CmisSync/Windows/TransmissionWPF.xaml.cs
+++ CmisSync/Windows/TransmissionWPF.xaml.cs
@@ -16,59 +16,51 @@
 //
 // </copyright>
 //-----------------------------------------------------------------------
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Diagnostics;
+﻿
+namespace CmisSync {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
 
-namespace CmisSync
-{
     /// <summary>
     /// Interaction logic for TransmissionWPF.xaml
     /// </summary>
-    public partial class TransmissionWPF : UserControl
-    {
-        public TransmissionWPF()
-        {
+    public partial class TransmissionWPF: UserControl {
+        public TransmissionWPF() {
             InitializeComponent();
             ApplyTransmission();
         }
 
-        private void ApplyTransmission()
-        {
+        private void ApplyTransmission() {
             ColumnRepo.Header = Properties_Resources.TransmissionTitleRepo;
             ColumnPath.Header = Properties_Resources.TransmissionTitlePath;
             ColumnStatus.Header = Properties_Resources.TransmissionTitleStatus;
             ColumnProgress.Header = Properties_Resources.TransmissionTitleProgress;
-            OkButton.Content = Properties_Resources.Finish;
+            OkButton.Content = Properties_Resources.Close;
         }
 
-        private void ListViewItem_Open(object sender, RoutedEventArgs e)
-        {
-            foreach (object item in ListView.SelectedItems)
-            {
+        private void ListViewItem_Open(object sender, RoutedEventArgs e) {
+            foreach (object item in ListView.SelectedItems) {
                 Transmission.TransmissionData data = item as Transmission.TransmissionData;
-                if (data.Done)
-                {
+                if (data.Done) {
                     Process.Start(data.FullPath);
                 }
             }
         }
 
-        private void ListViewItem_OpenFileLocation(object sender, RoutedEventArgs e)
-        {
-            foreach (object item in ListView.SelectedItems)
-            {
+        private void ListViewItem_OpenFileLocation(object sender, RoutedEventArgs e) {
+            foreach (object item in ListView.SelectedItems) {
                 Transmission.TransmissionData data = item as Transmission.TransmissionData;
                 Process.Start("explorer.exe", "/select,\"" + data.FullPath + "\"");
             }
