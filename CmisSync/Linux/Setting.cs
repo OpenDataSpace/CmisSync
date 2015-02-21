826cf91819090341d6f03faeab9ac65769b0e671
Till Lorentzen <till.lorentzen@graudata.com>
2015-02-21 13:09:02 +0100

    Whitespaces

--- CmisSync/Linux/Setting.cs
+++ CmisSync/Linux/Setting.cs
@@ -16,15 +16,13 @@
 //
 // </copyright>
 //-----------------------------------------------------------------------
-using CmisSync.Lib.Config;
 
-
-namespace CmisSync
-{
+namespace CmisSync {
     using System;
 
-    public partial class Setting : Gtk.Window
-    {
+    using CmisSync.Lib.Config;
+
+    public partial class Setting : Gtk.Window {
         private SettingController Controller = new SettingController();
         public Setting() : 
                 base(Gtk.WindowType.Toplevel)
@@ -68,8 +66,7 @@ namespace CmisSync
             this.saveButton.Sensitive = false;
         }
 
-        protected void OnNotificationToggleButtonToggled(object sender, EventArgs e)
-        {
+        protected void OnNotificationToggleButtonToggled(object sender, EventArgs e) {
             this.saveButton.Sensitive = this.proxyWidget.IsValid;
         }
     }
