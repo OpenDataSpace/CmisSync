726970725aa894fc8f8c4ea4d3d29d6cc7a73d44
Till Lorentzen <till.lorentzen@graudata.com>
2015-03-11 10:24:40 +0100

    Added copyright informations

--- CmisSync/TestLibrary/IntegrationTests/PrivateWorkingCopyTests/CreateDocumentTests.cs
+++ CmisSync/TestLibrary/IntegrationTests/PrivateWorkingCopyTests/CreateDocumentTests.cs
@@ -1,3 +1,21 @@
+//-----------------------------------------------------------------------
+// <copyright file="CreateDocumentTests.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace TestLibrary.IntegrationTests.PrivateWorkingCopyTests {
     using System;
@@ -22,14 +40,14 @@ namespace TestLibrary.IntegrationTests.PrivateWorkingCopyTests {
 
             var doc = this.remoteRootDir.CreateDocument(this.fileName, (string)null, checkedOut: true);
             this.remoteRootDir.Refresh();
-            doc.SetContent(content);
+            doc.SetContent(this.content);
             var newObjectId = doc.CheckIn(true, null, null, string.Empty);
             var newDocument = this.session.GetObject(newObjectId) as IDocument;
 
             this.remoteRootDir.Refresh();
-            Assert.That(this.remoteRootDir.GetChildren().First().Name, Is.EqualTo(fileName));
-            Assert.That(newDocument.Name, Is.EqualTo(fileName));
-            Assert.That(newDocument.ContentStreamLength, Is.EqualTo(content.Length));
+            Assert.That(this.remoteRootDir.GetChildren().First().Name, Is.EqualTo(this.fileName));
+            Assert.That(newDocument.Name, Is.EqualTo(this.fileName));
+            Assert.That(newDocument.ContentStreamLength, Is.EqualTo(this.content.Length));
         }
 
         [Test, Category("Slow"), MaxTime(180000)]
