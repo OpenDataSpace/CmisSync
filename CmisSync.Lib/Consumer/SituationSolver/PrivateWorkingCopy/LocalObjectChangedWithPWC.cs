73cef9af2de66c8c9d58db2b51a8b7e8c331f698
Till Lorentzen <till.lorentzen@graudata.com>
2015-03-12 21:21:44 +0100

    Added template for local object changed solver with PWC support
    This is also a decorator class which should implement checkout and checkin as well as passing all other calls to given decorated solver

---
+++ CmisSync.Lib/Consumer/SituationSolver/PrivateWorkingCopy/LocalObjectChangedWithPWC.cs
@@ -0,0 +1,54 @@
+//-----------------------------------------------------------------------
+// <copyright file="LocalObjectChangedWithPWC.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CmisSync.Lib.Consumer.SituationSolver.PWC {
+    using System;
+
+    using CmisSync.Lib.Events;
+    using CmisSync.Lib.Queueing;
+    using CmisSync.Lib.Storage.Database;
+    using CmisSync.Lib.Storage.FileSystem;
+
+    using DotCMIS.Client;
+
+    using log4net;
+
+    public class LocalObjectChangedWithPWC : AbstractEnhancedSolver {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(LocalObjectChangedWithPWC));
+        private ISolver folderOrFileContentUnchangedSolver;
+
+        public LocalObjectChangedWithPWC(
+            ISession session,
+            IMetaDataStorage storage,
+            IFileTransmissionStorage transmissionStorage,
+            ActiveActivitiesManager manager,
+            ISolver folderOrFileContentUnchangedSolver) : base(session, storage, transmissionStorage) {
+            throw new NotImplementedException();
+        }
+
+        public override void Solve(
+            IFileSystemInfo localFileSystemInfo,
+            IObjectId remoteId,
+            ContentChangeType localContent = ContentChangeType.NONE,
+            ContentChangeType remoteContent = ContentChangeType.NONE)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
