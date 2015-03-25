9d5ded0613726c837f813fd806f3b3731e9b3488
Till Lorentzen <till.lorentzen@graudata.com>
2015-03-25 10:41:24 +0100

    Added test template for abortable stream

---
+++ CmisSync/TestLibrary/StreamsTests/AbortableStreamTest.cs
@@ -0,0 +1,15 @@
+
+namespace TestLibrary.StreamsTests {
+    using System;
+
+    using CmisSync.Lib.Streams;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AbortableStreamTest {
+
+    }
+}
