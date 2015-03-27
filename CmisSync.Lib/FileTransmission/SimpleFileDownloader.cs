b713b199325704e6e02ffe3617bf21874ca17652
Till Lorentzen <till.lorentzen@graudata.com>
2015-03-27 11:38:08 +0100

    Fixed compile error

--- CmisSync.Lib/FileTransmission/SimpleFileDownloader.cs
+++ CmisSync.Lib/FileTransmission/SimpleFileDownloader.cs
@@ -101,7 +101,7 @@ namespace CmisSync.Lib.FileTransmission
 
                         HashAlgorithmReuse reuse = hashAlg as HashAlgorithmReuse;
                         if (reuse != null && update != null) {
-                            using (HashAlgorithm hash = reuse.GetHashAlgorithm()) {
+                            using (HashAlgorithm hash = (HashAlgorithm)reuse.Clone()) {
                                 hash.TransformFinalBlock(new byte[0], 0, 0);
                                 update(hash.Hash);
                             }
