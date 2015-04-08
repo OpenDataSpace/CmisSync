e0219ad8741f7449d1c26e3b21d8d27dcf8774de
Till Lorentzen <till.lorentzen@graudata.com>
2015-04-08 12:46:21 +0200

    Reduced whitespaces and did code formattings

--- CmisSync/Mac/SetupSubLoginController.cs
+++ CmisSync/Mac/SetupSubLoginController.cs
@@ -16,133 +16,121 @@
 //
 // </copyright>
 //-----------------------------------------------------------------------
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Globalization;
-using System.Threading;
-using MonoMac.Foundation;
-using MonoMac.AppKit;
 
-using CmisSync.Lib.Config;
-using CmisSync.Lib.Cmis;
-using CmisSync.Lib.Cmis.UiUtils;
+namespace CmisSync {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Globalization;
+    using System.Threading;
+    using MonoMac.Foundation;
+    using MonoMac.AppKit;
 
-namespace CmisSync
-{
-    public partial class SetupSubLoginController : MonoMac.AppKit.NSViewController
-    {
+    using CmisSync.Lib.Config;
+    using CmisSync.Lib.Cmis;
+    using CmisSync.Lib.Cmis.UiUtils;
 
+    public partial class SetupSubLoginController : MonoMac.AppKit.NSViewController {
         #region Constructors
 
         // Called when created from unmanaged code
-        public SetupSubLoginController (IntPtr handle) : base (handle)
-        {
-            Initialize ();
+        public SetupSubLoginController(IntPtr handle) : base(handle) {
+            Initialize();
         }
+
         // Called when created directly from a XIB file
-        [Export ("initWithCoder:")]
-        public SetupSubLoginController (NSCoder coder) : base (coder)
-        {
-            Initialize ();
+        [Export("initWithCoder:")]
+        public SetupSubLoginController(NSCoder coder) : base(coder) {
+            Initialize();
         }
+
         // Call to load from the XIB/NIB file
-        public SetupSubLoginController (SetupController controller) : base ("SetupSubLogin", NSBundle.MainBundle)
-        {
+        public SetupSubLoginController(SetupController controller) : base("SetupSubLogin", NSBundle.MainBundle) {
             this.Controller = controller;
-            Initialize ();
+            Initialize();
         }
+
         // Shared initialization code
-        void Initialize ()
-        {
+        void Initialize() {
         }
 
         #endregion
 
-        protected override void Dispose (bool disposing)
-        {
-            base.Dispose (disposing);
-            Console.WriteLine (this.GetType ().ToString () + " disposed " + disposing.ToString ());
+        protected override void Dispose(bool disposing) {
+            base.Dispose(disposing);
+            Console.WriteLine(this.GetType().ToString() + " disposed " + disposing.ToString());
         }
 
-        public override void AwakeFromNib ()
-        {
-            base.AwakeFromNib ();
+        public override void AwakeFromNib() {
+            base.AwakeFromNib();
 
             this.AddressLabel.StringValue = Properties_Resources.EnterWebAddress;
             this.UserLabel.StringValue = Properties_Resources.User;
             this.PasswordLabel.StringValue = Properties_Resources.Password;
 
-            this.AddressDelegate = new TextFieldDelegate ();
+            this.AddressDelegate = new TextFieldDelegate();
             this.AddressText.Delegate = this.AddressDelegate;
 
             this.ContinueButton.Title = Properties_Resources.Continue;
             this.CancelButton.Title = Properties_Resources.Cancel;
 
-            this.AddressText.StringValue = (Controller.PreviousAddress == null || String.IsNullOrEmpty (Controller.PreviousAddress.ToString ())) ? "https://" : Controller.PreviousAddress.ToString ();
-            this.UserText.StringValue = String.IsNullOrEmpty (Controller.saved_user) ? Environment.UserName : Controller.saved_user;
+            this.AddressText.StringValue = (Controller.PreviousAddress == null || string.IsNullOrEmpty(Controller.PreviousAddress.ToString())) ? "https://" : Controller.PreviousAddress.ToString();
+            this.UserText.StringValue = string.IsNullOrEmpty(Controller.saved_user) ? Environment.UserName : Controller.saved_user;
 //            this.PasswordText.StringValue = String.IsNullOrEmpty (Controller.saved_password) ? "" : Controller.saved_password;
-            this.PasswordText.StringValue = "";
+            this.PasswordText.StringValue = string.Empty;
 
-            InsertEvent ();
+            InsertEvent();
 
             //  Must be called after InsertEvent()
-            CheckAddressTextField ();
+            CheckAddressTextField();
         }
 
-        void InsertEvent()
-        {
+        void InsertEvent() {
             this.AddressDelegate.StringValueChanged += CheckAddressTextField;
             Controller.UpdateSetupContinueButtonEvent += SetContinueButton;
             Controller.UpdateAddProjectButtonEvent += SetContinueButton;
         }
 
-        void RemoveEvent()
-        {
+        void RemoveEvent() {
             this.AddressDelegate.StringValueChanged -= CheckAddressTextField;
             Controller.UpdateSetupContinueButtonEvent -= SetContinueButton;
             Controller.UpdateAddProjectButtonEvent -= SetContinueButton;
         }
 
-        void SetContinueButton(bool enabled)
-        {
-            InvokeOnMainThread (delegate
-            {
+        void SetContinueButton(bool enabled) {
+            InvokeOnMainThread(delegate {
                 ContinueButton.Enabled = enabled;
 //                ContinueButton.KeyEquivalent = "\r";
             });
         }
 
-        void CheckAddressTextField()
-        {
-            InvokeOnMainThread (delegate
-            {
-                string error = Controller.CheckAddPage (AddressText.StringValue);
-                if (String.IsNullOrEmpty (error))
-                    AddressHelp.StringValue = "";
-                else
-                    AddressHelp.StringValue = Properties_Resources.ResourceManager.GetString (error, CultureInfo.CurrentCulture);
+        void CheckAddressTextField() {
+            InvokeOnMainThread(delegate {
+                string error = Controller.CheckAddPage(AddressText.StringValue);
+                if (string.IsNullOrEmpty(error)) {
+                    AddressHelp.StringValue = string.Empty;
+                } else {
+                    AddressHelp.StringValue = Properties_Resources.ResourceManager.GetString(error, CultureInfo.CurrentCulture);
+                }
             });
         }
 
         SetupController Controller;
         TextFieldDelegate AddressDelegate;
 
-        partial void OnCancel (MonoMac.Foundation.NSObject sender)
-        {
+        partial void OnCancel(MonoMac.Foundation.NSObject sender) {
             RemoveEvent();
             Controller.PageCancelled();
         }
 
-        partial void OnContinue (MonoMac.Foundation.NSObject sender)
-        {
+        partial void OnContinue(MonoMac.Foundation.NSObject sender) {
             ServerCredentials credentials = new ServerCredentials() {
                 UserName = UserText.StringValue,
                 Password = PasswordText.StringValue,
                 Address = new Uri(AddressText.StringValue),
                 Binding = (Controller.saved_binding == null) ? ServerCredentials.BindingBrowser : Controller.saved_binding
             };
-            WarnText.StringValue = String.Empty;
+            WarnText.StringValue = string.Empty;
             AddressText.Enabled = false;
             UserText.Enabled = false;
             PasswordText.Enabled = false;
@@ -171,12 +159,12 @@ namespace CmisSync
                         RemoveEvent();
                         Controller.Add1PageCompleted(result.Credentials.Address, result.Credentials.Binding, credentials.UserName, credentials.Password.ToString());
                     }
+
                     LoginProgress.StopAnimation(this);
                 });
             });
             check.Start();
         }
-
 
         //strongly typed view accessor
         public new SetupSubLogin View {
@@ -186,4 +174,3 @@ namespace CmisSync
         }
     }
 }
-
