509672b61a9cb75b3c832c434eaf354c84510c29
Till Lorentzen <till.lorentzen@graudata.com>
2015-04-10 11:08:20 +0200

    Added Mac OS Icon names

--- CmisSync.Lib/Cmis/UiUtils/Icons.cs
+++ CmisSync.Lib/Cmis/UiUtils/Icons.cs
@@ -5,8 +5,148 @@ namespace CmisSync.Lib.Cmis.UiUtils {
     public enum Icons {
         [LinuxIcon("dataspacesync-app")]
         [WindowsIcon("dataspacesync", "ico")]
-        [MacOSIcon("dataspacesync", "icns")]
-        APPLICATION_ICON
+        [MacOSIcon("cmissync-app", "icns")]
+        ApplicationIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("cmissync-folder", "icns")]
+        DefaultTargetFolderIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("cmissync-folder", "icns")]
+        FolderIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-i")]
+        RemoteFolderIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-i")]
+        SyncInProgressIcon1,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-ii")]
+        SyncInProgressIcon2,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-iii")]
+        SyncInProgressIcon3,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-iiii")]
+        SyncInProgressIcon4,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-iiiii")]
+        SyncInProgressIcon5,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-i-active")]
+        SyncInProgressHighlightedIcon1,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-ii-active")]
+        SyncInProgressHighlightedIcon2,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-iii-active")]
+        SyncInProgressHighlightedIcon3,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-iiii-active")]
+        SyncInProgressHighlightedIcon4,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-iiiii-active")]
+        SyncInProgressHighlightedIcon5,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-error")]
+        ErrorOnSyncIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("process-syncing-error-active")]
+        ErrorOnSyncHighlightedIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("about")]
+        AboutImage,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("side-splash")]
+        SideImage,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("error", "icns")]
+        ErrorIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("tutorial-slide-1")]
+        TutorialSlide1,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("tutorial-slide-2")]
+        TutorialSlide2,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("tutorial-slide-3")]
+        TutorialSlide3,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("tutorial-slide-4")]
+        TutorialSlide4,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("media_playback_start")]
+        ResumeIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("media_playback_pause")]
+        PauseIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("Uploading")]
+        UploadNewObjectIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("Updating")]
+        UploadAndUpdateExistingObjectIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("Downloading")]
+        DownloadNewObjectIcon,
+
+        [LinuxIcon("")]
+        [WindowsIcon("")]
+        [MacOSIcon("Updating")]
+        DownloadAndUpdateExistingObjectIcon,
     }
 
     public class LinuxIcon: IconAttribute {
