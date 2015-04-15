5ad633f56f3db7b49daa8f9125855dac5f59753a
more <more0401@gmail.com>
2015-04-15 17:35:15 +0800

    Framework for monomac NSTableCellView

--- CmisSync/Mac/TransmissionWidgetItem.designer.cs
+++ CmisSync/Mac/TransmissionWidgetItem.designer.cs
@@ -1,10 +1,33 @@
-﻿
-namespace CmisSync {
-    
-    // Should subclass MonoMac.AppKit.NSView
-    [MonoMac.Foundation.Register("TransmissionWidgetItem")]
-    public partial class TransmissionWidgetItem {
-    }
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoMac.Foundation;
+using System.CodeDom.Compiler;
+
+namespace CmisSync
+{
+	[Register ("TransmissionWidgetItem")]
+	partial class TransmissionWidgetItem
+	{
+		[Outlet]
+		public MonoMac.AppKit.NSProgressIndicator progress { get; private set; }
+		
+        [Outlet]
+        public MonoMac.AppKit.NSTextField label { get; private set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (progress != null) {
+				progress.Dispose ();
+				progress = null;
+			}
+            if (label != null) {
+                label.Dispose ();
+                label = null;
+            }
+		}
+	}
 }
-
-
