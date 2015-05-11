4b0575c3171063d7f5d952a9510b9352a4ef814d
Till Lorentzen <till.lorentzen@graudata.com>
2015-05-11 16:53:00 +0200

    Added first simple implementation of a db verification tool for all configured repos

--- DancingQueen/Program.cs
+++ DancingQueen/Program.cs
@@ -1,9 +1,27 @@
 
 namespace DancingQueen {
     using System;
+    using System.Collections.Generic;
+
+    using CmisSync.Lib.Config;
+    using CmisSync.Lib.PathMatcher;
+    using CmisSync.Lib.Storage.Database;
+
+    using DBreeze;
+
+    using log4net;
+
     class MainClass {
         public static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var config = ConfigManager.CurrentConfig;
+
+            foreach (var repoInfo in config.Folders) {
+                using (var dbEngine = new DBreezeEngine(repoInfo.GetDatabasePath())) {
+                    var storage = new MetaDataStorage(dbEngine, new PathMatcher(repoInfo.LocalPath, repoInfo.RemotePath));
+                    Console.WriteLine(string.Format("Checking {0} and DB Path \"{1}\"", repoInfo.DisplayName, repoInfo.GetDatabasePath()));
+                    storage.ValidateObjectStructure();
+                }
+            }
         }
     }
 }
