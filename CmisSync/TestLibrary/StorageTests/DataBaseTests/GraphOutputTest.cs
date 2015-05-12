5c6b422d7b9f55f3a88109938252b5bb43a79ba8
Till Lorentzen <till.lorentzen@graudata.com>
2015-05-12 18:28:13 +0200

    Added unit test for local file tree output writer

--- CmisSync/TestLibrary/StorageTests/DataBaseTests/GraphOutputTest.cs
+++ CmisSync/TestLibrary/StorageTests/DataBaseTests/GraphOutputTest.cs
@@ -14,8 +14,18 @@ namespace TestLibrary.StorageTests.DataBaseTests {
     [TestFixture]
     public class GraphOutputTest {
         [Test, Category("Fast")]
-        public void WritingTreeToStream() {
-            var underTest = this.CreateTree();
+        public void WritingStringTreeToStream() {
+            var underTest = this.CreateStringTree();
+            using (var stream = new MemoryStream()) {
+                underTest.ToDotStream(stream);
+                var content = System.Text.Encoding.Default.GetString(stream.ToArray());
+                Console.WriteLine(content);
+            }
+        }
+
+        [Test, Category("Fast")]
+        public void WritingFileTreeToStream() {
+            var underTest = this.CreateFileTree();
             using (var stream = new MemoryStream()) {
                 underTest.ToDotStream(stream);
                 var content = System.Text.Encoding.Default.GetString(stream.ToArray());
@@ -25,7 +35,7 @@ namespace TestLibrary.StorageTests.DataBaseTests {
 
         [Test, Category("Fast")]
         public void WritingTreeToFileInfo() {
-            var underTest = this.CreateTree();
+            var underTest = this.CreateStringTree();
             var file = new Mock<IFileInfo>();
             file.Setup(f => f.Open(System.IO.FileMode.CreateNew)).Returns(() => new MemoryStream());
 
@@ -34,11 +44,20 @@ namespace TestLibrary.StorageTests.DataBaseTests {
             file.Verify(f => f.Open(FileMode.CreateNew), Times.Once());
         }
 
-        private IObjectTree<string> CreateTree() {
+        private IObjectTree<string> CreateStringTree() {
             var tree = new ObjectTree<string>() { Item = "root" };
             var children = new List<IObjectTree<string>>();
             children.Add(new ObjectTree<string>() { Item = "A" });
             children.Add(new ObjectTree<string>() { Item = "B" });
+            tree.Children = children;
+            return tree;
+        }
+
+        private IObjectTree<IFileSystemInfo> CreateFileTree() {
+            var tree = new ObjectTree<IFileSystemInfo>() { Item = Mock.Of<IFileSystemInfo>(m => m.Name == "root") };
+            var children = new List<IObjectTree<IFileSystemInfo>>();
+            children.Add(new ObjectTree<IFileSystemInfo>() { Item = Mock.Of<IFileSystemInfo>(m => m.Name == "A") });
+            children.Add(new ObjectTree<IFileSystemInfo>() { Item = Mock.Of<IFileSystemInfo>(m => m.Name == "B") });
             tree.Children = children;
             return tree;
         }
