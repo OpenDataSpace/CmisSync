f5d19c85e59cfe1eef49b031de8e3c5364a3e110
Till Lorentzen <till.lorentzen@graudata.com>
2015-05-15 16:19:47 +0200

    Changed Name from DancingQueen to DiagnoseTool

--- DiagnoseTool/Program.cs
+++ DiagnoseTool/Program.cs
@@ -24,12 +24,12 @@ namespace DancingQueen {
                     var storage = new MetaDataStorage(dbEngine, new PathMatcher(repoInfo.LocalPath, repoInfo.RemotePath), false);
                     Console.WriteLine(string.Format("Checking {0} and DB Path \"{1}\"", repoInfo.DisplayName, repoInfo.GetDatabasePath()));
                     storage.ValidateObjectStructure();
-                    var treeBuilder = new DescendantsTreeBuilder(storage, null, null, null, null);
+                    /*var treeBuilder = new DescendantsTreeBuilder(storage, null, null, null, null);
                     Console.WriteLine(string.Format("Creating local, stored and remote tree in \"{0}\"", Path.GetTempPath()));
                     var trees = treeBuilder.BuildTrees();
                     trees.LocalTree.ToDotFile(Path.Combine(Path.GetTempPath(), "LocalTree.dot"));
                     trees.StoredTree.ToDotFile(Path.Combine(Path.GetTempPath(), "StoredTree.dot"));
-                    trees.RemoteTree.ToDotFile(Path.Combine(Path.GetTempPath(), "RemoteTree.dot"));
+                    trees.RemoteTree.ToDotFile(Path.Combine(Path.GetTempPath(), "RemoteTree.dot"));*/
                 }
             }
         }
