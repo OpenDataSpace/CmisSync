040e610c7ed1abfaaf15b436e6d6428fb1fa6d4e
Till Lorentzen <till.lorentzen@graudata.com>
2015-05-15 11:52:09 +0200

    Fixed compile error and added new output functions

--- DancingQueen/Program.cs
+++ DancingQueen/Program.cs
@@ -2,12 +2,16 @@
 namespace DancingQueen {
     using System;
     using System.Collections.Generic;
+    using System.IO;
 
     using CmisSync.Lib.Config;
     using CmisSync.Lib.PathMatcher;
+    using CmisSync.Lib.Producer.Crawler;
     using CmisSync.Lib.Storage.Database;
 
     using DBreeze;
+
+    using DotCMIS.Client;
 
     using log4net;
 
@@ -17,9 +21,15 @@ namespace DancingQueen {
 
             foreach (var repoInfo in config.Folders) {
                 using (var dbEngine = new DBreezeEngine(repoInfo.GetDatabasePath())) {
-                    var storage = new MetaDataStorage(dbEngine, new PathMatcher(repoInfo.LocalPath, repoInfo.RemotePath));
+                    var storage = new MetaDataStorage(dbEngine, new PathMatcher(repoInfo.LocalPath, repoInfo.RemotePath), false);
                     Console.WriteLine(string.Format("Checking {0} and DB Path \"{1}\"", repoInfo.DisplayName, repoInfo.GetDatabasePath()));
                     storage.ValidateObjectStructure();
+                    var treeBuilder = new DescendantsTreeBuilder(storage, null, null, null, null);
+                    Console.WriteLine(string.Format("Creating local, stored and remote tree in \"{0}\"", Path.GetTempPath()));
+                    var trees = treeBuilder.BuildTrees();
+                    trees.LocalTree.ToDotFile(Path.Combine(Path.GetTempPath(), "LocalTree.dot"));
+                    trees.StoredTree.ToDotFile(Path.Combine(Path.GetTempPath(), "StoredTree.dot"));
+                    trees.RemoteTree.ToDotFile(Path.Combine(Path.GetTempPath(), "RemoteTree.dot"));
                 }
             }
         }
