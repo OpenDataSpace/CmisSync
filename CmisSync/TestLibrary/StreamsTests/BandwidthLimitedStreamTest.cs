adc40545628bce161a6ae212a53d26cff41779dc
Till Lorentzen <till.lorentzen@graudata.com>
2015-06-29 12:45:16 +0200

    Renamed unit test to fit better to the test context

--- CmisSync/TestLibrary/StreamsTests/BandwidthLimitedStreamTest.cs
+++ CmisSync/TestLibrary/StreamsTests/BandwidthLimitedStreamTest.cs
@@ -80,7 +80,7 @@ namespace TestLibrary.StreamsTests {
         }
 
         [Test, Category("Slow"), Category("Streams"), Timeout(10200)]
-        public void ReadOrWriteOneByteMoreThanLimitTakesAtMinimumOneSecond([Values(true, false)]bool read) {
+        public void LimitReadOrWriteStream([Values(true, false)]bool read) {
             using (var sourceOrTargetStream  = new MemoryStream(new byte[this.length]))
             using (var wrappedStream = new MemoryStream(this.buffer))
             using (var monitorStream = new BandwidthNotifyingStream(wrappedStream))
