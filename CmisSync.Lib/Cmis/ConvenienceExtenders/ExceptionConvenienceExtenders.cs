45aae4ea0b6c0e3a2b6ff3d5ca6de92a640d8f67
Till Lorentzen <till.lorentzen@graudata.com>
2015-07-03 10:25:39 +0200

    Added convenience extender for detecting a virus exception

---
+++ CmisSync.Lib/Cmis/ConvenienceExtenders/ExceptionConvenienceExtenders.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CmisSync.Lib.Cmis.ConvenienceExtenders {
+    using System;
+
+    using DotCMIS.Exceptions;
+
+    public static class ExceptionConvenienceExtenders {
+        public static bool IsVirusDetectionException(this CmisConstraintException ex) {
+            if (ex.ErrorContent.Contains("Virus")) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
