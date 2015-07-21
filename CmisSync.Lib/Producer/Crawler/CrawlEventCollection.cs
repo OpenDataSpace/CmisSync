dc30e0b49428de9cea41538aefef73891dddaa02
Till Lorentzen <till.lorentzen@graudata.com>
2015-07-21 16:43:18 +0200

    Reduced whitespaces

--- CmisSync.Lib/Producer/Crawler/CrawlEventCollection.cs
+++ CmisSync.Lib/Producer/Crawler/CrawlEventCollection.cs
@@ -16,16 +16,15 @@
 //
 // </copyright>
 //-----------------------------------------------------------------------
-namespace CmisSync.Lib.Producer.Crawler
-{
+
+namespace CmisSync.Lib.Producer.Crawler {
     using System;
     using System.Collections.Generic;
 
     using CmisSync.Lib.Events;
     using CmisSync.Lib.Storage.FileSystem;
 
-    public struct CrawlEventCollection
-    {
+    public struct CrawlEventCollection {
         public List<AbstractFolderEvent> creationEvents;
         public Dictionary<string, Tuple<AbstractFolderEvent, AbstractFolderEvent>> mergableEvents;
     }
