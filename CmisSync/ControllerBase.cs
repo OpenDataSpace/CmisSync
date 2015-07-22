fdea69a3cc039d4c3182dee722e463b4101e72b6
Till Lorentzen <till.lorentzen@graudata.com>
2015-07-22 16:18:42 +0200

    Fixed on demand branding failure

--- CmisSync/ControllerBase.cs
+++ CmisSync/ControllerBase.cs
@@ -608,7 +608,7 @@ namespace CmisSync {
                         string pathname = Path.Combine(config.GetConfigPath(), this.BrandConfigFolder, path.Substring(1));
                         Directory.CreateDirectory(Path.GetDirectoryName(pathname));
                         try {
-                            using (FileStream output = File.Open(pathname, FileMode.Truncate, FileAccess.Write)) {
+                            using (FileStream output = File.Open(pathname, FileMode.Create, FileAccess.Write)) {
                                 if (!clientBrand.GetFile(path, output)) {
                                     success = false;
                                     break;
