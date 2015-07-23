f76ddb4cbe0dd8bd1976c698809a26a16b036c0f
Till Lorentzen <till.lorentzen@graudata.com>
2015-07-23 10:27:51 +0200

    Added read only property as reason for a change event

--- CmisSync.Lib/Producer/Crawler/RemoteEventGenerator.cs
+++ CmisSync.Lib/Producer/Crawler/RemoteEventGenerator.cs
@@ -16,8 +16,8 @@
 //
 // </copyright>
 //-----------------------------------------------------------------------
-namespace CmisSync.Lib
-{
+
+namespace CmisSync.Lib.Producer.Crawler {
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -28,12 +28,10 @@ namespace CmisSync.Lib
     using CmisSync.Lib.Storage.Database.Entities;
 
     using DotCMIS.Client;
-    public class RemoteEventGenerator
-    {
+    public class RemoteEventGenerator {
         private IMetaDataStorage storage;
 
-        public RemoteEventGenerator(IMetaDataStorage storage)
-        {
+        public RemoteEventGenerator(IMetaDataStorage storage) {
             if (storage == null) {
                 throw new ArgumentNullException("storage");
             }
@@ -85,14 +83,17 @@ namespace CmisSync.Lib
             return createdEvents;
         }
 
-        private AbstractFolderEvent CreateRemoteEventBasedOnStorage(IFileableCmisObject cmisObject, IMappedObject storedParent, IMappedObject storedMappedChild)
+        private AbstractFolderEvent CreateRemoteEventBasedOnStorage(
+            IFileableCmisObject cmisObject,
+            IMappedObject storedParent,
+            IMappedObject storedMappedChild)
         {
             AbstractFolderEvent newEvent = null;
             if (storedParent != null && storedMappedChild.ParentId == storedParent.RemoteObjectId) {
                 // Renamed or Equal
                 if (storedMappedChild.Name == cmisObject.Name) {
                     // Equal or property update
-                    if (storedMappedChild.LastChangeToken != cmisObject.ChangeToken) {
+                    if (storedMappedChild.LastChangeToken != cmisObject.ChangeToken || storedMappedChild.IsReadOnly != cmisObject.IsReadOnly()) {
                         // Update
                         newEvent = FileOrFolderEventFactory.CreateEvent(cmisObject, null, MetaDataChangeType.CHANGED, src: this);
                         AddRemoteContentChangeTypeToFileEvent(newEvent as FileEvent, storedMappedChild, cmisObject as IDocument);
