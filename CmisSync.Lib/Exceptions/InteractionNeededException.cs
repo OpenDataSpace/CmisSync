577e72f01e036e96e0c214bdc69f76182c58a2bb
Till Lorentzen <till.lorentzen@graudata.com>
2015-07-27 17:18:20 +0200

    Refactoring of InterationNeededException
    InterationNeededException splited up to abstract and implementation part

--- CmisSync.Lib/Exceptions/InteractionNeededException.cs
+++ CmisSync.Lib/Exceptions/InteractionNeededException.cs
@@ -30,80 +30,30 @@ namespace CmisSync.Lib.Exceptions {
     /// Interaction needed exception. This exception should be thrown if a user must be informed about a needed interaction to solve a problem/conflict.
     /// </summary>
     [Serializable]
-    public class InteractionNeededException : Exception {
+    public class InteractionNeededException : AbstractInteractionNeededException {
         /// <summary>
-        /// Initializes a new instance of the <see cref="CmisSync.Lib.Consumer.InteractionNeededException"/> class.
+        /// Initializes a new instance of the <see cref="CmisSync.Lib.Exceptions.InteractionNeededException"/> class.
         /// </summary>
-        public InteractionNeededException() : base() {
-            this.InitParams();
-        }
+        public InteractionNeededException() : base() { }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="CmisSync.Lib.Consumer.InteractionNeededException"/> class.
+        /// Initializes a new instance of the <see cref="CmisSync.Lib.Exceptions.InteractionNeededException"/> class.
         /// </summary>
         /// <param name="msg">Exception message.</param>
-        public InteractionNeededException(string msg) : base(msg) {
-            this.InitParams();
-        }
+        public InteractionNeededException(string msg) : base(msg) { }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="CmisSync.Lib.Consumer.InteractionNeededException"/> class.
+        /// Initializes a new instance of the <see cref="CmisSync.Lib.Exceptions.InteractionNeededException"/> class.
         /// </summary>
         /// <param name="msg">Exception message.</param>
         /// <param name="inner">Inner exception.</param>
-        public InteractionNeededException(string msg, Exception inner) : base(msg, inner) {
-            this.InitParams();
-        }
+        public InteractionNeededException(string msg, Exception inner) : base(msg, inner) { }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="CmisSync.Lib.Consumer.InteractionNeededException"/> class.
+        /// Initializes a new instance of the <see cref="CmisSync.Lib.Exceptions.InteractionNeededException"/> class.
         /// </summary>
         /// <param name="info">Serialization info.</param>
         /// <param name="context">Streaming context.</param>
-        protected InteractionNeededException(SerializationInfo info, StreamingContext context) : base(info, context) {
-        }
-
-        /// <summary>
-        /// Gets or sets the title.
-        /// </summary>
-        /// <value>The title.</value>
-        public string Title { get; set; }
-
-        /// <summary>
-        /// Gets or sets the description.
-        /// </summary>
-        /// <value>The description.</value>
-        public string Description { get; set; }
-
-        /// <summary>
-        /// Gets or sets the details. The details are technical details, such as error messages from server or a stack trace.
-        /// </summary>
-        /// <value>The details of the problem.</value>
-        public string Details { get; set; }
-
-        /// <summary>
-        /// Gets the actions, which can be executed to solve the problem.
-        /// </summary>
-        /// <value>The actions.</value>
-        public Dictionary<string, Action> Actions { get; private set; }
-
-        /// <summary>
-        /// Gets the affected files. If local files or folders are involved to the problem, they should be listed.
-        /// </summary>
-        /// <value>The affected files.</value>
-        public List<IFileSystemInfo> AffectedFiles { get; private set; }
-
-        private void InitParams() {
-            this.AffectedFiles = new List<IFileSystemInfo>();
-            this.Actions = new Dictionary<string, Action>();
-            this.Title = this.GetType().Name;
-            this.Description = this.Message;
-            this.Details = string.Empty;
-            if (this.InnerException is CmisBaseException) {
-                this.Details = (this.InnerException as CmisBaseException).ErrorContent;
-            } else if (this.InnerException != null) {
-                this.Details = this.InnerException.StackTrace ?? string.Empty;
-            }
-        }
+        protected InteractionNeededException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
