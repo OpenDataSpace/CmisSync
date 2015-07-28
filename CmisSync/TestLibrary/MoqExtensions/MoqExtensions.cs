d227b06560b5e6133ed13e01c06b30db146b555c
Till Lorentzen <till.lorentzen@graudata.com>
2015-07-28 17:24:17 +0200

    Improved code style

--- CmisSync/TestLibrary/MoqExtensions/MoqExtensions.cs
+++ CmisSync/TestLibrary/MoqExtensions/MoqExtensions.cs
@@ -24,11 +24,11 @@
 using System.Collections.Generic;
 using Moq.Language.Flow;
 
-public static class MoqExtensions
-{
+public static class MoqExtensions {
     public static void ReturnsInOrder<T, TResult>(
         this ISetup<T, TResult> setup, 
-        params TResult[] results) where T : class {
-    setup.Returns(new Queue<TResult>(results).Dequeue);
+        params TResult[] results) where T : class
+    {
+        setup.Returns(new Queue<TResult>(results).Dequeue);
     }
 }
