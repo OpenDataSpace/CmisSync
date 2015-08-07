5809bcf9a7a8a7775812efd666cba9864f8d1d97
Till Lorentzen <till.lorentzen@graudata.com>
2015-08-07 12:29:42 +0200

    Added Toxiproxy.Net to TestLibrary
    All other projects must be updated to .Net 4.5 for Toxiproxy.Net support

---
+++ CmisSync/TestLibrary/IntegrationTests/NetworkFailuresTests/ToxiProxySetup.cs
@@ -0,0 +1,41 @@
+﻿
+namespace TestLibrary.IntegrationTests.NetworkFailuresTests {
+    using System;
+    using System.Linq;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    using TestLibrary.TestUtils;
+
+    using Toxiproxy.Net;
+
+    [TestFixture, Category("FailureIT"), Ignore("https://github.com/mdevilliers/Toxiproxy.Net/pull/6")]
+    public class ToxyProxySetup {
+        [Test]
+        public void SetupConnectionToProxy() {
+            using (var conn = new Connection()) {
+                var client = conn.Client();
+                foreach (var proxyName in client.All().Keys) {
+                    client.Delete(proxyName);
+                }
+
+                var proxy = client.Add(new Proxy() {
+                    Enabled = true,
+                    Name = "local_to_devel",
+                    Upstream = "devel.dataspace.cc:80",
+                    Listen = "127.0.0.1:8080"
+                });
+                var bandwidth = client.FindProxy(proxy.Name).UpStreams().BandwidthToxic;
+                bandwidth.Enabled = true;
+                bandwidth.Rate = 1000;
+                bandwidth.Update();
+                bandwidth = client.FindProxy(proxy.Name).DownStreams().BandwidthToxic;
+                bandwidth.Enabled = true;
+                bandwidth.Rate = 1000;
+                bandwidth.Update();
+            }
+        }
+    }
+}
