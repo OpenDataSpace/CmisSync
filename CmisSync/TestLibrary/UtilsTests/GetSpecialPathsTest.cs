e518d800c8feb5121ee7763def17a778cee98145
Till Lorentzen <till.lorentzen@graudata.com>
2015-08-19 10:18:11 +0200

    Added unit test to figure out if special folders are supported

---
+++ CmisSync/TestLibrary/UtilsTests/GetSpecialPathsTest.cs
@@ -0,0 +1,20 @@
+﻿
+namespace TestLibrary.UtilsTests {
+    using System;
+
+    using NUnit.Framework;
+
+    [TestFixture, Category("Fast")]
+    public class GetSpecialPathsTest {
+        [Test, TestCaseSource("GetAllFolder")]
+        public void SpecialFolder(Environment.SpecialFolder folder) {
+            string path = Environment.GetFolderPath(folder, Environment.SpecialFolderOption.DoNotVerify);
+            Assert.That(path, Is.Not.Null);
+            Console.WriteLine(path);
+        }
+
+        public Array GetAllFolder(){
+            return Enum.GetValues(typeof(Environment.SpecialFolder));
+        }
+    }
+}
