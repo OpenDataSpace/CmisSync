6b327bdafb7e8ad9af43a112766e2e0eaa4960dd
Till Lorentzen <till.lorentzen@graudata.com>
2015-09-07 14:30:02 +0200

    Added alpha fs lib via nuget

---
+++ CmisSync/TestLibrary/StorageTests/AlphaFileSystemTests/FileInfoWithLongPathTest.cs
@@ -0,0 +1,16 @@
+﻿
+namespace TestLibrary {
+    using System;
+
+    using Alphaleonis.Win32.Filesystem;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FileInfoWithLongPathTest {
+        [Test, Category("Medium")]
+        public void TestCase() {
+            var file = new FileInfo("test");
+        }
+    }
+}
