eacf545fadbb9f4e452a03ab204f8e84926e02c2
Till Lorentzen <till.lorentzen@graudata.com>
2015-09-08 10:08:23 +0200

    Added first simple integration tests

--- CmisSync/TestLibrary/StorageTests/AlphaFileSystemTests/FileInfoWithLongPathTest.cs
+++ CmisSync/TestLibrary/StorageTests/AlphaFileSystemTests/FileInfoWithLongPathTest.cs
@@ -9,8 +9,27 @@ namespace TestLibrary.StorageTests.AlphaFSTests {
     [TestFixture]
     public class FileInfoWithLongPathTest {
         [Test, Category("Medium")]
-        public void TestCase() {
-            var file = new FileInfo("test");
+        public void CreateInstance() {
+            var file = new FileInfo(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()), PathFormat.FullPath);
+            Assert.That(file.Exists, Is.False);
+        }
+
+        [Test, Category("Medium")]
+        public void WriteADS() {
+            var fileName = Guid.NewGuid().ToString();
+            var adsName = "DSS-Test";
+            File.WriteAllText(Path.Combine(Path.GetTempPath(), fileName + ":" + adsName, PathFormat.FullPath), fileName);
+
+            var file = new FileInfo(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()) , PathFormat.FullPath);
+            Assert.That(file.Exists, Is.True);
+            foreach (var stream in file.EnumerateAlternateDataStreams()) {
+                Assert.That(stream.StreamName, Is.EqualTo(string.Empty).Or.EqualTo(adsName));
+                if (stream.StreamName == adsName) {
+                    Assert.That(stream.Size, Is.EqualTo(fileName.Length));
+                } else {
+                    Assert.That(stream.Size, Is.EqualTo(0));
+                }
+            }
         }
     }
 }
