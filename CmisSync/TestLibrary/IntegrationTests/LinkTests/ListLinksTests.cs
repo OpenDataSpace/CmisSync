82ca3d62c849bbcd5d87b8af3bcb1cae2d35fbc2
Till Lorentzen <till.lorentzen@graudata.com>
2016-02-03 11:50:13 +0100

    Added copyright header

--- CmisSync/TestLibrary/IntegrationTests/LinkTests/ListLinksTests.cs
+++ CmisSync/TestLibrary/IntegrationTests/LinkTests/ListLinksTests.cs
@@ -1,7 +1,22 @@
-﻿using NUnit.Framework.Constraints;
-
-
-namespace TestLibrary.IntegrationTests.LinkTests {
+//-----------------------------------------------------------------------
+// <copyright file="ListLinksTests.cs" company="GRAU DATA AG">
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General private License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//   GNU General private License for more details.
+//
+//   You should have received a copy of the GNU General private License
+//   along with this program. If not, see http://www.gnu.org/licenses/.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+﻿namespace TestLibrary.IntegrationTests.LinkTests {
     using System;
     using System.Collections.Generic;
 
@@ -11,6 +26,7 @@ namespace TestLibrary.IntegrationTests.LinkTests {
     using DotCMIS.Exceptions;
 
     using NUnit.Framework;
+    using NUnit.Framework.Constraints;
 
     [TestFixture, Timeout(180000), TestName("ListLinks")]
     public class ListLinksTests : BaseLinkTest {
